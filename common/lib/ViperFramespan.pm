17aaf61050fff5f20e28e8fbea4b9c82a7104a04
Martial Michel <martial.michel@nist.gov>
2008-04-18 20:34:10 +0000

    - Can get a non decimal number when converting a frame to ts (especialy if it is the result of a middlepoint) - Added file header

--- common/lib/ViperFramespan.pm
+++ common/lib/ViperFramespan.pm
@@ -1,5 +1,22 @@
 package ViperFramespan;
 
+# Viper Framespan
+#
+# Author(s): Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "ViperFramespan.pm" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+
 # $Id$
 
 use strict;
@@ -599,7 +616,8 @@ sub frame_to_ts {
     return(0);
   }
 
-  if ($frame !~ m%^\d+$%) {
+
+  if ($frame !~ m%^[\d\.]+$%) {
     $self->_set_errormsg($error_msgs{"NotAFrame"});
     return(0);
   }
