1df706d4bd942156ac052a449417c54844c49f56
Martial Michel <martial.michel@nist.gov>
2008-04-18 20:34:32 +0000

    Added header

--- TrecVid08/lib/TrecVid08ViperFile.pm
+++ TrecVid08/lib/TrecVid08ViperFile.pm
@@ -1,5 +1,22 @@
 package TrecVid08ViperFile;
 
+# TrecVid08 ViperFile
+#
+# Author(s): Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "TrecVid08ViperFile.pm" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+
 # $Id$
 
 use strict;
