40285c6231c26253d7f5230cfe30ac3cf21e6e1a
Martial Michel <martial.michel@nist.gov>
2008-04-22 23:04:34 +0000

    Started work toward having the merger be able to perform frame shift and overlap processing

--- TrecVid08/tools/TV08MergeHelper/TV08MergeHelper.pl
+++ TrecVid08/tools/TV08MergeHelper/TV08MergeHelper.pl
@@ -50,6 +50,17 @@ unless (eval "use TrecVid08ViperFile; 1")
     $have_everything = 0;
   }
 
+# TrecVid08EventList (part of this tool)
+unless (eval "use TrecVid08EventList; 1")
+  {
+    warn_print
+      (
+       "\"TrecVid08EventList\" is not available in your Perl installation. ",
+       "It should have been part of this tools' files."
+      );
+    $have_everything = 0;
+  }
+
 # Getopt::Long (usualy part of the Perl Core)
 unless (eval "use Getopt::Long; 1")
   {
@@ -85,8 +96,10 @@ my $usage = &set_usage();
 my $isgtf = 0; # a Ground Truth File is authorized not to have the Decision informations set
 my $xmllint = "";
 my $xsdpath = ".";
-my $writeto = "";
+my $writetodir = "";
 my $fps = -1;
+my $forceFilename = "";
+my $checkOverlap = 0;
 my $show = 0;
 
 # Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
@@ -102,8 +115,10 @@ GetOptions
    'xmllint=s'       => \$xmllint,
    'TrecVid08xsd=s'  => \$xsdpath,
    'gtf'             => \$isgtf,
-   'writeto=s'       => \$writeto,
+   'writetodir=s'    => \$writetodir,
    'fps=s'           => \$fps,
+   'ForceFilename=s' => \$forceFilename,
+   'overlap'         => \$checkOverlap,
    # Hidden option
    'show_internals+' => \$show,
   ) or error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
@@ -114,7 +129,9 @@ die("$versionid\n") if ($opt{'version'});
 die("\n$usage\n") if (scalar @ARGV == 0);
 
 die("ERROR: \'fps\' must set in order to do be able to use \'observations\' objects\n\n$usage") if ($fps == -1);
-die("ERROR: No \'writeto\' set, aborting\n\n$usage\n") if ($writeto =~ m%^\s*$%);
+die("ERROR: No \'writeto\' set, aborting\n\n$usage\n") if ($writetodir =~ m%^\s*$%);
+
+die("ERROR: \'ForceFilename\' option selected but no value set\n$usage") if (($opt{'ForceFilename'}) && ($forceFilename eq ""));
 
 if ($xmllint ne "") {
   error_quit("While trying to set \'xmllint\' (" . $dummy->get_errormsg() . ")")
@@ -126,92 +143,155 @@ if ($xsdpath ne "") {
     if (! $dummy->set_xsdpath($xsdpath));
 }
 
-##########
+error_quit("Problem with \'writetodir\': $!")
+  if ((! -e $writetodir) || (! -d $writetodir));
+
+##############################
 # Main processing
 my $tmp;
-my $mergefile = undef;
 my $ntodo = scalar @ARGV;
 my $ndone = 0;
+my %all_vf;
+my %all_fs_shift;
 
+##########
+print "\n\n** STEP 1: Load all files to be merged\n";
 while ($tmp = shift @ARGV) {
-  if (! -e $tmp) {
-    &valerr($tmp, "file does not exists, skipping");
-    next;
-  }
-  if (! -f $tmp) {
-    &valerr($tmp, "is not a file, skipping\n");
-    next;
-  }
-  if (! -r $tmp) {
-    &valerr($tmp, "file is not readable, skipping\n");
-    next;
+  my $fname = $tmp;
+  my $fs_shift = 0;
+
+  if ($tmp =~ m%^(.+?)\:(\d+)$%) {
+    $fname = $1;
+    $fs_shift = $2;
   }
 
-  # Prepare the object
-  my $object = new TrecVid08ViperFile();
-  error_quit("While trying to set \'xmllint\' (" . $object->get_errormsg() . ")")
-    if ( ($xmllint ne "") && (! $object->set_xmllint($xmllint)) );
-  error_quit("While trying to set \'TrecVid08xsd\' (" . $object->get_errormsg() . ")")
-    if ( ($xsdpath ne "") && (! $object->set_xsdpath($xsdpath)) );
-  error_quit("While setting \'gtf\' status (" . $object->get_errormsg() . ")")
-    if ( ($isgtf) && ( ! $object->set_as_gtf()) );
-  error_quit("While setting \'file\' (" . $object->get_errormsg() . ")")
-    if ( ! $object->set_file($tmp) );
-  error_quit("While setting \'fps\' ($fps) (" . $object->get_errormsg() . ")")
-    if ( ! $object->set_fps($fps) );
+  my ($ok, $object) = &load_file($isgtf, $fname);
+  next if (! $ok);
 
-  # Validate
-  if (! $object->validate()) {
-    &valerr($tmp, $object->get_errormsg());
-    next;
-  } else {
-    &valok($tmp, "loaded");
-  }
+  $all_vf{$fname} = $object;
+  $all_fs_shift{$fname} = $fs_shift;
+  $ndone++;
 
   if ($show > 1) {
-    print "** FILE: $tmp\n";
+    print "** FILE: $fname\n";
     print $object->_display();
   }
+}
+print "* -> Loaded $ndone ok / $ntodo\n";
+error_quit("Could not succesfully load all files processed, aborting\n")
+  if ($ndone != $ntodo);
+error_quit("No file loaded, aborting\n")
+  if ($ndone == 0);
 
-  # Create the mergefile object if not existant yet
-  if (! defined $mergefile) {
-    $mergefile = $object->clone_with_no_events();
-    error_quit("While duplicating the first object (" . $object->get_errormsg() .")")
+##########
+print "\n\n** STEP 2: Process all observations\n";
+
+my %mergefiles;
+my $EL = new TrecVid08EventList();
+my ($adone, $akept);
+error_quit("Problem creating the EventList (" . $EL->get_errormsg() . ")")
+  if ($EL->error());
+
+
+foreach my $key (keys %all_vf) {
+  my $object = $all_vf{$key};
+
+  if ($forceFilename ne "") {
+    $object->change_sourcefile_filename($forceFilename);
+    error_quit("Problem while changing the sourcefile filename (" . $object->get_errormsg() .")")
       if ($object->error());
   }
+
+  # Get the sourcefile filename
+  my $sffn = $object->get_sourcefile_filename();
+  error_quit("Problem obtaining the sourcefile filename (" . $object->get_errormsg() .")")
+    if ($object->error());
+
+  # Create the mergefile object for this sourcefile filename (if not existant yet)
+  if (! defined $mergefiles{$sffn}) {
+    my $mf = $object->clone_with_no_events();
+    error_quit("While duplicating the source object (" . $object->get_errormsg() .")")
+      if ($object->error());
+    $mergefiles{$sffn} = $mf;
+  }
+
+  # Get the observation list for this viper file
+  my @ao = $object->get_all_events_observations();
+  error_quit("While duplicating the source object (" . $object->get_errormsg() .")")
+    if ($object->error());
+  my @kept = ();
+
+  # Timeshift
+  if ($all_fs_shift{$key} != 0) {
+  }
+
+  # Overlap
+  if ($checkOverlap) {
+  } else {
+    @kept = @ao;
+  }
+  
+  # Add the observations to the EventList
+  $EL->add_Observations(@kept);
+  error_quit("Problem adding Observations to EventList (" . $EL->get_errormsg() . ")")
+    if ($EL->error());
+
+  print "- Done processing Observations from '$key' (Found: ", scalar @ao, (($checkOverlap) ? (" / Kept: " . scalar @kept) : ""), ")\n";
+  $adone += scalar @ao;
+  $akept += scalar @kept;
+}
+print "* -> Found $adone Observations", (($checkOverlap) ? " (Kept: $akept)" : ""), "\n";
+
+
+##########
+print "\n\n** STEP 3: Writting merge file(s)\n";
+
+my $fdone;
+my $ftodo = scalar keys %mergefiles;
+foreach my $key (keys %mergefiles) {
+  my $mf = $mergefiles{$key};
 
   # Now add all observations from the current file to the output file
   foreach my $i (@ok_events) {
-    my @bucket = $object->get_event_observations($i);
-    error_quit("While \'get_event_observations\' (" . $object->get_errormsg() .")")
-      if ($object->error());
+    my @bucket = $EL->get_Observations_list($key, $i);
+    error_quit("While obatining Observations list ($key / $i) (" . $EL->get_errormsg() .")")
+      if ($EL->error());
     foreach my $obs (@bucket) {
-      $mergefile->add_observation($obs);
-      error_quit("While \'add_observation\' (" . $mergefile->get_errormsg() .")")
-	if ($mergefile->error());
+      # Add a comment to the observation to indicate where it came from
+      my $comment = "Was originally: " . $obs->get_unique_id();
+      $obs->addto_comment($comment);
+      error_quit("While adding a comment to observation (" . $obs->get_errormsg() .")")
+	if ($obs->error());
+      
+      $mf->add_observation($obs);
+      error_quit("While \'add_observation\' (" . $mf->get_errormsg() .")")
+	if ($mf->error());
     }
   }
+  if ($show) {
+    print "** MERGED FILE MEMORY REPRESENTATION:\n";
+    print $mf->_display();
+  }
 
-  $ndone++;
+  my $txt = $mf->reformat_xml();
+  error_quit("While trying to re-represent XML (" . $mf->get_errormsg() . ")")
+    if ($mf->error());
+
+  my $writeto = "$writetodir/$key.xml";
+  open WRITETO, ">$writeto"
+    or error_quit("Could not create output XML file ($writeto): $!\n");
+  print WRITETO $txt;
+  close WRITETO;
+  print "Wrote: $writeto\n";
+
+  $fdone++;
 }
-error_quit("Could not succesfully load all files processed (Loaded: $ndone / $ntodo)\n")
-  if ($ndone != $ntodo);
+print "* -> Wrote $fdone files (out of $ftodo)\n";
 
-if ($show) {
-  print "** MERGED FILE MEMORY REPRESENTATION:\n";
-  print $mergefile->_display();
-}
+error_quit("Not all file could be written, aborting")
+  if ($fdone != $ftodo);
 
-my $txt = $mergefile->reformat_xml();
-error_quit("While trying to \'write\' (" . $mergefile->get_errormsg() . ")")
-  if ($mergefile->error());
-open WRITETO, ">$writeto"
-  or error_quit("Could not create output file ($writeto): $!\n");
-print WRITETO $txt;
-close WRITETO;
-print "Wrote: $writeto\n";
-
-die("Done.\n");
+die("\nDone.\n");
 
 ########## END
 
@@ -237,7 +317,7 @@ sub set_usage {
   my $tmp=<<EOF
 $versionid
 
-Usage: $0 [--help] [--version] [--gtf] [--xmllint location] [--TrecVid08xsd location] viper_source_file.xml [viper_source_file.xml [...]] --writeto file
+Usage: $0 [--help] [--version] [--gtf] [--xmllint location] [--TrecVid08xsd location] viper_source_file.xml[:frame_shift] [viper_source_file.xml[:frame_shift] [...]] --fps fps --writetodir dir
 
 Will perform a semantic validation of the Viper XML file(s) provided.
 
@@ -245,8 +325,10 @@ Will perform a semantic validation of the Viper XML file(s) provided.
   --gtf           Specify that the file to validate is a Ground Truth File
   --xmllint       Full location of the \'xmllint\' executable
   --TrecVid08xsd  Path where the XSD files can be found ($xsdfiles)
-  --writeto       Once processed in memory, print a new XML dump of file read
+  --writetodir    Once processed in memory, print the new XML dump files to this directory
   --fps           Set the number of frames per seconds (float value) (also recognined: PAL, NTSC)
+  --ForceFilename Specify that all files loaded refers to the same 'sourcefile' file
+  --overlap       Consider framespan shift and overlap processing
   --version       Print version number and exit
   --help          Print this usage information and exit
 
@@ -281,4 +363,47 @@ sub ok_quit {
 
   print "\n";
   exit(0);
+}
+
+########################################
+
+
+sub load_file {
+  my ($isgtf, $tmp) = @_;
+
+  if (! -e $tmp) {
+    &valerr($tmp, $isgtf, "file does not exists, skipping");
+    return(0, ());
+  }
+  if (! -f $tmp) {
+    &valerr($tmp, $isgtf, "is not a file, skipping\n");
+    return(0, ());
+  }
+  if (! -r $tmp) {
+    &valerr($tmp, $isgtf, "file is not readable, skipping\n");
+    return(0, ());
+  }
+  
+  # Prepare the object
+  my $object = new TrecVid08ViperFile();
+  error_quit("While trying to set \'xmllint\' (" . $object->get_errormsg() . ")")
+    if ( ($xmllint ne "") && (! $object->set_xmllint($xmllint)) );
+  error_quit("While trying to set \'TrecVid08xsd\' (" . $object->get_errormsg() . ")")
+    if ( ($xsdpath ne "") && (! $object->set_xsdpath($xsdpath)) );
+  error_quit("While setting \'gtf\' status (" . $object->get_errormsg() . ")")
+    if ( ($isgtf) && ( ! $object->set_as_gtf()) );
+  error_quit("While setting \'fps\' ($fps) (" . $object->get_errormsg() . ")")
+    if ( ! $object->set_fps($fps) );
+  error_quit("While setting \'file\' ($tmp) (" . $object->get_errormsg() . ")")
+    if ( ! $object->set_file($tmp) );
+  
+  # Validate (important to confirm that we can have a memory representation)
+  if (! $object->validate()) {
+    &valerr($tmp, $object->get_errormsg());
+    return(0, ());
+  }
+
+  &valok($tmp, "Loaded");
+  
+  return(1, $object);
 }
