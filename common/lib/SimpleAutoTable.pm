d92f656a66a0a29f3302e9c5a35e98acfd8e7f20
Jonathan Fiscus <jonathan.fiscus@nist.gov>
2008-05-22 16:36:18 +0000

    Major revision to using the property list.  Delegated to the object PropList object.

--- common/lib/SimpleAutoTable.pm
+++ common/lib/SimpleAutoTable.pm
@@ -6,18 +6,12 @@ use Data::Dumper;
 use strict;
 
 my $key_KeyColumn = "KeyColumn";
-my @av_KeyColumn = ("Keep", "Remove"); # Order is Important
+#my @av_KeyColumn = ("Keep", "Remove"); # Order is Important
 my $key_SortRowKey = "SortRowKey";
-my @av_SortRowKey = ("AsAdded", "Num", "Alpha"); # Order is Important
+#my @av_SortRowKey = ("AsAdded", "Num", "Alpha"); # Order is Important
 
 sub new {
   my ($class) = shift @_;
-
-  my $prop = new PropList();
-  $prop->addProp($key_KeyColumn, $av_KeyColumn[0], @av_KeyColumn);
-  $prop->addProp($key_SortRowKey, $av_SortRowKey[0], @av_SortRowKey);
-
-  my $errormsg = &_set_errormsg_txt("", $prop->get_errormsg());
 
   my $self =
     {
@@ -36,11 +30,16 @@ sub new {
       SubID        => {},
       width        => { icgMult => 0, icgSepMult => 0, charLen => 0 },
      },
-     Properties  => $prop,
-     errormsg    => $errormsg,
+     Properties  => new PropList(),
+     errormsg    => "",
     };
 
   bless $self;
+
+  $self->{Properties}->addProp($key_KeyColumn, "Keep", ("Keep", "Remove"));
+  $self->{Properties}->addProp($key_SortRowKey, "AsAdded", ("AsAdded", "Num", "Alpha"));
+  $self->_set_errormsg($self->{Properties}->get_errormsg());
+
   return($self);
 }
 
@@ -64,124 +63,66 @@ sub get_errormsg {
 
 #####
 
-sub _set_errormsg_txt {
-  my ($oh, $add) = @_;
-
-  my $txt = "$oh$add";
-
-  $txt =~ s%\[SimpleAutoTable\]\s+%%g;
-
-  return("") if (&_is_blank($txt));
-
-  $txt = "[SimpleAutoTable] $txt";
-
-  return($txt);
-}
-
-#####
-
 sub _set_errormsg {
   my ($self, $txt) = @_;
 
-  $self->{errormsg} = &_set_errormsg_txt($self->{errormsg}, $txt);
+  my $newTxt = $self->{errormsg}.$txt;
+
+  $newTxt =~ s%\[SimpleAutoTable\]\s+%%g;
+
+  if (&_is_blank($newTxt)){
+    $newTxt = "";
+  } else {
+    $newTxt = "[SimpleAutoTable] $txt";
+  }
+  
+  $self->{errormsg} = $newTxt;
 }
 
 ##########
 
-sub getProp_KeyColumn {
-  my ($self) = @_;
-
-  my $prop = $self->{Properties};
-
-  my $v = $prop->getValue($key_KeyColumn);
-  if ($prop->error()) {
-    $self->_set_erromsg("Could not get the KeyColumn Property (" . $prop->get_errormsg() . ")");
-    return(0);
-  }
-  
-  return($v);
+sub setProperties(){
+    my ($self, $propHT) = @_;
+    
+    if (! $self->{Properties}->setValueFromHash($propHT)){
+        $self->_set_erromsg("Could not set Properties: ",$self->{Properties}->get_errormsg());
+        return (0);
+    }
+    return (1);
 }
+    
 
-#####
-
-sub _setProp_KeyColumn {
-  my ($self, $val) = @_;
-
-  my $prop = $self->{Properties};
-
-  if ((! $prop->setValue($key_KeyColumn, $val)) || ($prop->error())) {
-    $self->_set_erromsg("Could not set the KeyColumn Property (" . $prop->get_errormsg() . ")");
-    return(0);
-  }
-
-  return(1);
-}
-
-#####
-
-sub setProp_KeepKeyColumn {
-  my ($self) = @_;
-  return($self->_setProp_KeyColumn($av_KeyColumn[0]));
-}
-
-#####
-
-sub setProp_RemoveKeyColumn {
-  my ($self) = @_;
-  return($self->_setProp_KeyColumn($av_KeyColumn[1]));
-}
-
-##########
-
-sub getProp_SortRowKey {
-  my ($self) = @_;
-
-  my $prop = $self->{Properties};
-
-  my $v = $prop->getValue($key_SortRowKey);
-  if ($prop->error()) {
-    $self->_set_erromsg("Could not get the SortRowKey Property (" . $prop->get_errormsg() . ")");
-    return(0);
-  }
-  
-  return($v);
-}
-
-#####
-
-sub _setProp_SortRowKey {
-  my ($self, $val) = @_;
-
-  my $prop = $self->{Properties};
-
-  if ((! $prop->setValue($key_SortRowKey, $val)) || ($prop->error())) {
-    $self->_set_erromsg("Could not set the SortRowKey Property (" . $prop->get_errormsg() . ")");
-    return(0);
-  }
-
-  return(1);
-}
-
-#####
-
-sub setProp_SortRowKeyAsAdded {
-  my ($self) = @_;
-  return($self->_setProp_SortRowKey($av_SortRowKey[0]));
-}
-
-#####
-
-sub setProp_SortRowKeyNum {
-  my ($self) = @_;
-  return($self->_setProp_SortRowKey($av_SortRowKey[1]));
-}
-
-#####
-
-sub setProp_SortRowKeyAlpha {
-  my ($self) = @_;
-  return($self->_setProp_SortRowKey($av_SortRowKey[2]));
-}
+############
+##
+##sub _getProp_KeyColumn {
+##  my ($self) = @_;
+##
+##  my $prop = $self->{Properties};
+##
+##  my $v = $prop->getValue($key_KeyColumn);
+##  if ($prop->error()) {
+##    $self->_set_erromsg("Could not get the KeyColumn Property (" . $prop->get_errormsg() . ")");
+##    return(0);
+##  }
+##  
+##  return($v);
+##}
+##
+############
+##
+##sub _getProp_SortRowKey {
+##  my ($self) = @_;
+##
+##  my $prop = $self->{Properties};
+##
+##  my $v = $prop->getValue($key_SortRowKey);
+##  if ($prop->error()) {
+##    $self->_set_erromsg("Could not get the SortRowKey Property (" . $prop->get_errormsg() . ")");
+##    return(0);
+##  }
+##  
+##  return($v);
+##}
 
 ##########
 
@@ -276,14 +217,23 @@ sub _getColLabelWidth(){
 }
 
 sub _getOrderedLabelIDs(){
-    my ($self, $ht) = @_;
+    my ($self, $ht, $order) = @_;
     my @ids = ();
-        
-    my @sortedKeys = sort { $ht->{SubID}{$a}->{thisIDNum} <=> $ht->{SubID}{$b}->{thisIDNum}} keys %{ $ht->{SubID} };
-    
+            
+    my @sortedKeys = ();
+    if ($order eq "AsAdded"){
+        @sortedKeys = sort { $ht->{SubID}{$a}->{thisIDNum} <=> $ht->{SubID}{$b}->{thisIDNum}} keys %{ $ht->{SubID} };
+    } elsif ($order eq "Num") {
+        @sortedKeys = sort { $a <=> $b} keys %{ $ht->{SubID} };
+    } elsif ($order eq "Alpha") {
+        @sortedKeys = sort keys %{ $ht->{SubID} };
+    } else {
+        die "Internal Error SimpleAutoTable: Sort order '$order' not defined";
+    }  
+
     foreach my $sid(@sortedKeys){
         if ($ht->{SubID}->{$sid}->{SubIDCount} > 0){
-            foreach my $labelID ($self->_getOrderedLabelIDs($ht->{SubID}->{$sid})){
+            foreach my $labelID ($self->_getOrderedLabelIDs($ht->{SubID}->{$sid}), $order){
                 push @ids, "$sid|$labelID";
             }
         } else {
@@ -360,13 +310,17 @@ sub renderTxtTable(){
 #    print "Col num lev = $numColLev\n";
 #    print "Row num lev = $numRowLev\n";
 
-  my $r1c = (($self->getProp_KeyColumn()) eq $av_KeyColumn[1]) ? 1 : 0;
-  my $sm = $self->getProp_SortRowKey();
+  my ($keyColReturnCode, $keyCol) = $self->{Properties}->getValue($key_KeyColumn);
+  if (! $keyColReturnCode){
+     $self->_set_errormsg("Unable to get the ".$key_KeyColumn." property.  Message is ".$self->{Properties}->get_errormsg());
+     return(undef);
+  }
+  my $r1c = ($keyCol eq "Remove") ? 1 : 0;
     
   ### Compute the max width of the row labels for each level
   my $maxRowLabWidth = $interColGap;
   my @rowLabWidth = ();
-  for (my $rl= ($r1c ? 2 : 1); $rl <= $numRowLev; $rl++){
+  for (my $rl=1; $rl <= $numRowLev; $rl++){
     my $w = $self->_getRowLabelWidth($self->{rowLabOrder}, $rl);
     push @rowLabWidth, $w; 
     $maxRowLabWidth += $w + ($rl > 1 ? $interColGap : 0);    
@@ -377,8 +331,13 @@ sub renderTxtTable(){
   my ($r, $c, $fmt, $str, $rowIDStr, $colIDStr);
 
 #    print "The Report\n";
-  my @rowIDs = $self->_getOrderedLabelIDs($self->{"rowLabOrder"});
-  my @colIDs = $self->_getOrderedLabelIDs($self->{"colLabOrder"});
+  my ($rowReturnCode, $rowSort) = $self->{Properties}->getValue($key_SortRowKey);
+  if (! $rowReturnCode){
+     $self->_set_errormsg("Unable to to return get RowSort property.  Message is ".$self->{Properties}->get_errormsg());
+     return(undef);
+  }
+  my @rowIDs = $self->_getOrderedLabelIDs($self->{"rowLabOrder"}, $rowSort);
+  my @colIDs = $self->_getOrderedLabelIDs($self->{"colLabOrder"}, "AsAdded");
 #    print "ColIDs ".join(" ",@colIDs)."\n";
 
   ### Header output
@@ -401,32 +360,24 @@ sub renderTxtTable(){
 	   : "") . $self->_nChrStr($data_len, "-");
   $out .= "\n";
   
-  ### The data
-  my @srowIDs 
-    = ($sm eq $av_SortRowKey[0]) # As Added
-      ? @rowIDs 
-	: (($sm eq $av_SortRowKey[1]) # Num
-	   ? sort _num @rowIDs
-	   : sort @rowIDs); # Alpha
-
-  foreach $rowIDStr (@srowIDs){
+  foreach $rowIDStr (@rowIDs){
     if (! $r1c) {
-      for ($c=1; $c<=$numRowLev; $c++){
-	$out .= $self->_leftJust($rowIDStr, $maxRowLabWidth);
+     for ($c=1; $c<=$numRowLev; $c++){
+  	    $out .= $self->_leftJust($rowIDStr, $maxRowLabWidth);
       }
       $out .= "|";
     }
 
     foreach $colIDStr (@colIDs){
       $out .= "$gapStr" 
-	. $self->_rightJust($self->{data}{$rowIDStr."-".$colIDStr}, 
-			    $self->_getColLabelWidth($colIDStr));
+ 	           . $self->_rightJust($self->{data}{$rowIDStr."-".$colIDStr}, 
+			                       $self->_getColLabelWidth($colIDStr));
     }
       
     $out .= "\n";
   }   
     
-  print($out);
+  $out;
 }
 
 ############################################################
