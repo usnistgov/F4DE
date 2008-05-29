11a65cab8fca62c932eb74b28173a8c9709e261e
Martial Michel <martial.michel@nist.gov>
2008-05-29 05:44:09 +0000

    Make use of TrecVid08xmllint

--- TrecVid08/test/Pre/pre_tests.pl
+++ TrecVid08/test/Pre/pre_tests.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use TV08TestCore;
+use TrecVid08xmllint;
 
 my $err = 0;
 
@@ -13,7 +14,7 @@ my @tocheck = ("Getopt::Long", "Data::Dumper", "File::Temp",
 my $ms = scalar @tocheck;
 foreach my $i (@tocheck) {
   print "- $i : ";
-  my $v = TV08TestCore::check_package($i);
+  my $v = MMisc::check_package($i);
   my $t = $v ? "ok" : "**missing**";
   print "$t\n";
   $ms -= $v;
@@ -28,16 +29,17 @@ if ($ms > 0) {
 ##########
 print "** Checking for xmllint:\n";
 my $xmllint_env = "TV08_XMLLINT";
-my $xmllint = TV08TestCore::_get_env_val($xmllint_env, "");
+my $xmllint = MMisc::get_env_val($xmllint_env, "");
 if ($xmllint ne "") {
   print "- using the one specified by the $xmllint_env environment variable ($xmllint)\n";
 }
 
 my $error = "";
 # Confirm xmllint is present and at least 2.6.30
-($xmllint, $error) = &_check_xmllint($xmllint);
-if (! &TV08TestCore::is_blank($error)) {
-  print "$error\n";
+my $xobj = new TrecVid08xmllint();
+$xobj->set_xmllint($xmllint);
+if ($xobj->error()) {
+  print $xobj->get_errormsg();
   print "After installing a suitable version, set the $xmllint_env environment variable to ensure the use of the proper version if it is not in your PATH\n";
   $err++;
 } else {
@@ -49,49 +51,6 @@ if (! &TV08TestCore::is_blank($error)) {
 error_quit("\nSome issues, fix before attempting to run make check again\n") if ($err);
 
 ok_quit("\n** Pre-requisite testing done\n\n");
-
-
-########################################
-
-sub _check_xmllint {
-  my $xmllint = shift @_;
-
-  # If none provided, check if it is available in the path
-  if ($xmllint eq "") {
-    my ($retcode, $stdout, $stderr) = &TV08TestCore::_do_system_call('which', 'xmllint');
-    return("", "Could not find a valid \'xmllint\' command in the PATH, aborting\n")
-      if ($retcode != 0);
-    $xmllint = $stdout;
-  }
-
-  $xmllint =~ s{^~([^/]*)}{$1?(getpwnam($1))[7]:($ENV{HOME} || $ENV{LOGDIR})}ex;
-
-  # Check that the file for xmllint exists and is an executable file
-  return("", "\'xmllint\' ($xmllint) does not exist, aborting\n")
-    if (! -e $xmllint);
-
-  return("", "\'xmllint\' ($xmllint) is not a file, aborting\n")
-    if (! -f $xmllint);
-
-  return("", "\'xmllint\' ($xmllint) is not executable, aborting\n")
-    if (! -x $xmllint);
-
-  # Now check that it actually is xmllint
-  my ($retcode, $stdout, $stderr) = &TV08TestCore::_do_system_call($xmllint, '--version');
-  return("", "\'xmllint\' ($xmllint) does not seem to be a valid \'xmllint\' command, aborting\n")
-    if ($retcode != 0);
-  
-  if ($stderr =~ m%using\s+libxml\s+version\s+(\d+)%) {
-    # xmllint print the command name followed by the version number
-    my $version = $1;
-    return("", "\'xmllint\' ($xmllint) version too old: requires at least 2.6.30 (ie 20630, installed $version), aborting\n")
-      if ($version <= 20630);
-  } else {
-    return("", "Could not confirm that \'xmllint\' is valid, aborting\n");
-  }
-
-  return($xmllint, "");
-}
 
 ####################
 
