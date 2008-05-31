84f6147782a2b1ffcfd39fe0e63af437415c6bd4
Martial Michel <martial.michel@nist.gov>
2008-05-31 00:06:56 +0000

    Can now load and represent the ECF file in memory

--- TrecVid08/lib/TrecVid08ECF.pm
+++ TrecVid08/lib/TrecVid08ECF.pm
@@ -1,6 +1,6 @@
 package TrecVid08ECF;
 
-# TrecVid08 ECF
+# TrecVid08 ECF XML Handler
 #
 # Author(s): Martial Michel
 #
@@ -21,6 +21,12 @@ package TrecVid08ECF;
 
 use strict;
 
+use ViperFramespan;
+use TrecVid08xmllint;
+use MErrorH;
+use MMisc;
+use Data::Dumper;
+
 my $version     = "0.1b";
 
 if ($version =~ m/b$/) {
@@ -30,24 +36,17 @@ if ($version =~ m/b$/) {
 
 my $versionid = "TrecVid08ECF.pm Version: $version";
 
-# "ViperFramespan.pm" (part of this program sources)
-use ViperFramespan;
-
-# "MErrorH.pm" (part of this program sources)
-use MErrorH;
-
-# "MMisc.pm" (part of this program sources)
-use MMisc;
-
-# For the '_display()' function
-use Data::Dumper;
-
-
 ########################################
-##########
 
 # Required XSD files
 my @xsdfilesl = ( "ecf.xsd" );
+# Default values to compare against (constant values)
+my $default_error_value = "default_error_value";
+
+my @required_excerpt_tags = ("filename", "begin", "duration"); # Order is important
+my @optional_excerpt_tags = ("sample_rate", "language", "source_type", ); # Order is important here too
+my $key_fs = "key_fs";
+my $key_ots = "key_ots";
 
 ## Constructor
 sub new {
@@ -55,14 +54,21 @@ sub new {
 
   my $errortxt = (scalar @_ > 0) ? "TrecVid08ECF does not accept parameters" : "";
 
+  my $xmllintobj = new TrecVid08xmllint();
+  $xmllintobj->set_xsdfilesl(@xsdfilesl);
+  $errortxt .= $xmllintobj->get_errormsg() if ($xmllintobj->error());
+
   my $errormsg = new MErrorH("TrecVid08ECF");
   $errormsg->set_errormsg($errortxt);
 
   my $self =
     {
-     xmllint        => "",
-     xsdpath        => "",
+     file           => "",
+     xmllintobj     => $xmllintobj,
+     duration       => 0,
+     dfps           => -1,
      fhash          => undef,
+     validated      => 0, # To confirm file was validated
      errormsg       => $errormsg,
     };
 
@@ -70,7 +76,23 @@ sub new {
   return($self);
 }
 
-#####
+####################
+
+sub get_version {
+  my ($self) = @_;
+
+  return($versionid);
+}
+
+########################################
+
+sub get_required_xsd_files_list {
+  my ($self) = @_;
+
+  return(-1) if ($self->error());
+
+  return(@xsdfilesl);
+}
 
 ########## 'xmllint'
 
@@ -79,15 +101,13 @@ sub set_xmllint {
 
   return(0) if ($self->error());
 
-  my $error = "";
-  # Confirm xmllint is present and at least 2.6.30
-  ($xmllint, $error) = &_check_xmllint($xmllint);
-  if (! MMisc::is_blank($error)) {
-    $self->_set_errormsg($error);
+  $self->{xmllintobj}->set_xmllint($xmllint);
+
+  if ($self->{xmllintobj}->error()) {
+    $self->_set_errormsg($self->{xmllintobj}->get_errormsg());
     return(0);
   }
   
-  $self->{xmllint} = $xmllint;
   return(1);
 }
 
@@ -98,9 +118,7 @@ sub _is_xmllint_set {
 
   return(0) if ($self->error());
 
-  return(1) if (! MMisc::is_blank($self->{xmllint}));
-
-  return(0);
+  return($self->{xmllintobj}->is_xmllint_set());
 }
 
 #####
@@ -115,7 +133,7 @@ sub get_xmllint {
     return(0);
   }
 
-  return($self->{xmllint});
+  return($self->{xmllintobj}->get_xmllint());
 }
 
 ########## 'xsdpath'
@@ -125,15 +143,12 @@ sub set_xsdpath {
 
   return(0) if ($self->error());
 
-  my $error = "";
-  # Confirm that the required xsdfiles are available
-  ($xsdpath, $error) = &_check_xsdfiles($xsdpath, @xsdfilesl);
-  if (! MMisc::is_blank($error)) {
-    $self->_set_errormsg($error);
+  $self->{xmllintobj}->set_xsdpath($xsdpath);
+  if ($self->{xmllintobj}->error()) {
+    $self->_set_errormsg($self->{xmllintobj}->get_errormsg());
     return(0);
   }
 
-  $self->{xsdpath} = $xsdpath;
   return(1);
 }
 
@@ -144,9 +159,7 @@ sub _is_xsdpath_set {
 
   return(0) if ($self->error());
 
-  return(1) if (! MMisc::is_blank($self->{xsdpath}));
-
-  return(0);
+  return($self->{xmllintobj}->is_xsdpath_set());
 }
 
 #####
@@ -161,90 +174,447 @@ sub get_xsdpath {
     return(0);
   }
 
-  return($self->{xsdpath});
+  return($self->{xmllintobj}->get_xsdpath());
+}
+
+########## 'file'
+
+sub set_file {
+  my ($self, $file) = @_;
+
+  return(0) if ($self->error());
+
+  if (! -e $file) {
+    $self->_set_errormsg("File does not exists ($file)");
+    return(0);
+  }
+  if (! -r $file) {
+    $self->set_errormsg("File is not readable ($file)");
+    return(0);
+  }
+  if (! -f $file) {
+    $self->set_errormsg("Parameter is not a file ($file)");
+    return(0);
+  }
+
+  $self->{file} = $file;
+  return(1);
+}
+
+#####
+
+sub _is_file_set {
+  my ($self) = @_;
+
+  return(0) if ($self->error());
+
+  return(0) if (MMisc::is_blank($self->{file}));
+
+  return(1);
+}
+
+#####
+
+sub get_file {
+  my ($self) = @_;
+
+  return(-1) if ($self->error());
+
+  if (! $self->_is_file_set()) {
+    $self->_set_errormsg("\'file\' is not set");
+    return(0);
+  }
+
+  return($self->{file});
+}
+
+########## 'default_fps'
+
+sub set_default_fps {
+  my ($self, $fps) = @_;
+
+  return(0) if ($self->error());
+
+  # use ViperFramespan to create the accepted value
+  my $fs_tmp = new ViperFramespan();
+  if (! $fs_tmp->set_fps($fps)) {
+    $self->_set_errormsg("While setting the file fps ($fps) error (" . $fs_tmp->get_errormsg() . ")");
+    return(0);
+  }
+  # And get it back
+  my $nfps = $fs_tmp->get_fps();
+  if ($fs_tmp->error()) {
+    $self->_set_errormsg("While obtaining back the default_fps ($fps) error (" . $fs_tmp->get_errormsg() . ")");
+    return(0);
+  }
+
+  $self->{dfps} = $nfps;
+  return(1);
+}
+
+#####
+
+sub is_default_fps_set {
+  my ($self) = @_;
+
+  return(0) if ($self->error());
+
+  return(0) if ($self->{dfps} == -1);
+
+  return(1);
+}
+
+#####
+
+sub get_default_fps {
+  my ($self) = @_;
+
+  return(-1) if ($self->error());
+
+  if (! $self->is_default_fps_set()) {
+    $self->_set_errormsg("\'default_fps\' is not set");
+    return(0);
+  }
+
+  return($self->{dfps});
+}
+
+########## 'validate'
+
+sub is_validated {
+  my ($self) = @_;
+
+  return(0) if ($self->error());
+
+  return(1) if ($self->{validated} == 1);
+
+  return(0);
+}
+
+#####
+
+sub validate {
+  my ($self) = @_;
+
+  return(0) if ($self->error());
+
+  # No need to re-validate if the file was already validated :)
+  return(1) if ($self->is_validated());
+
+  if (! $self->is_default_fps_set()) {
+    $self->_set_errormsg("Can not validate unless a \'default_fps\' is set");
+    return(0);
+  }
+
+  if (! $self->_is_file_set()) {
+    $self->_set_errormsg("No file set (use \'set_file\') before calling the \'validate\' function");
+    return(0);
+  }
+  my $ifile = $self->get_file();
+
+  if (! $self->_is_xmllint_set()) {
+    # We will try to set it up from PATH
+    return(0) if (! $self->set_xmllint());
+  }
+
+  if (! $self->_is_xsdpath_set()) {
+    # We will try to set it up from '.'
+    return(0) if (! $self->set_xsdpath("."));
+  }
+
+  # Load the XML through xmllint
+  my ($bigstring) = $self->{xmllintobj}->run_xmllint($ifile);
+  if ($self->{xmllintobj}->error()) {
+    $self->_set_errormsg($self->{xmllintobj}->get_errormsg());
+    return(0);
+  }
+  # No data from xmllint ?
+  if (MMisc::is_blank($bigstring)) {
+    $self->_set_errormsg("WEIRD: The XML data returned by xmllint seems empty");
+    return(0);
+  }
+  
+  my $res;
+  # Initial Cleanups, Checks and Global Values Extraction
+  ($res, $bigstring) = $self->_data_cleanup_globalextract($bigstring);
+  if (! MMisc::is_blank($res)) {
+    $self->_set_errormsg($res);
+    return(0);
+  }
+  
+  # Process the data part
+  ($res, $bigstring) = $self->_excerpt_list_processor($bigstring);
+  if (! MMisc::is_blank($res)) {
+    $self->_set_errormsg($res);
+    return(0);
+  }
+  
+  $self->{validated} = 1;
+  return(1);
 }
 
 ##########
 
-sub _run_xmllint {
-  my $xmllint = shift @_;
-  my $xsdpath = shift @_;
-  my $file = shift @_;
+sub _data_cleanup_globalextract {
+  my ($self, $bigstring) = @_;
 
-  $file =~ s{^~([^/]*)}{$1?(getpwnam($1))[7]:($ENV{HOME} || $ENV{LOGDIR})}ex;
+  # Remove all XML comments
+  $bigstring =~ s%\<\!\-\-.+?\-\-\>%%sg;
 
-  my ($retcode, $stdout, $stderr) = MMisc::do_system_call
-	($xmllint, "--path", "\"$xsdpath\"", "--schema", 
-	 $xsdpath . "/" . $xsdfilesl[0], $file);
+  # Remove <?xml ...?> header
+  return("Could not find a proper \'<?xml ... ?>\' header, skipping", $bigstring)
+    if (! ($bigstring =~ s%^\s*\<\?xml.+?\?\>%%is));
 
-  return("Problem validating file with \'xmllint\' ($stderr), aborting", "")
-    if ($retcode != 0);
+  # Remove <ecf ...> and </ecf> header and trailer
+  return("Could not find a proper \'ecf\' tag, aborting", $bigstring)
+    if (! MtXML::remove_xml_tags("ecf", \$bigstring));
 
-  return("", $stdout);
+  # Get the required source_signal_duration data (in the header usualy)
+  my $name = "source_signal_duration";
+  my $ssd = MtXML::get_named_xml_section($name, \$bigstring, $default_error_value);
+  return("Could not find the \'$name\' required data, aborting", $bigstring)
+    if ($ssd eq $default_error_value);
+  return("Could not remvoe the \'$name\' tag, aborting", $bigstring)
+    if (! MtXML::remove_xml_tags($name, \$ssd));
+  $self->{duration}=$ssd;
+
+  # Get the optional version informaion (in the header usualy)
+  $name = "version";
+  my $version = MtXML::get_named_xml_section($name, \$bigstring, $default_error_value);
+  if ($version ne $default_error_value) {
+    return("Could not remvoe the \'$name\' tag, aborting", $bigstring)
+      if (! MtXML::remove_xml_tags($name, \$version));
+    $self->{version}=$version;
+  }
+
+  # At this point, all we ought to have left is the '<excerpt_list>' content
+  $name = "excerpt_list";
+  return("After initial cleanup, we found more than just viper \'excerpt_list\', aborting", $bigstring)
+    if (! ( ($bigstring =~ m%^\s*\<excerpt_list>%is) 
+	    && ($bigstring =~ m%\<\/excerpt_list\>\s*$%is) ) );
+
+  return("", $bigstring);
 }
-
-########################################
-# xmllint check
-
 
 #####
 
-sub _check_xmllint {
-  my $xmllint = shift @_;
+sub _excerpt_list_processor {
+  my ($self, $string) = @_;
 
-  # If none provided, check if it is available in the path
-  if ($xmllint eq "") {
-    my ($retcode, $stdout, $stderr) = MMisc::do_system_call('which', 'xmllint');
-    return("", "Could not find a valid \'xmllint\' command in the PATH, aborting\n")
-      if ($retcode != 0);
-    $xmllint = $stdout;
+  # Remove the excerpt_list header/trailer tags
+  my $name = "excerpt_list";
+  return("Could not remvoe the \'$name\' tag, aborting", $string)
+      if (! MtXML::remove_xml_tags($name, \$string));  
+
+  ##### Process all that is left in the string (should only be excerpts)
+  $name = "excerpt";
+  while (! MMisc::is_blank($string)) {
+    my $sec = MtXML::get_named_xml_section($name, \$string, $default_error_value);
+    return("Found some content other than \'$name\'", $string)
+      if ($sec eq $default_error_value);
+
+    my $err = $self->_excerpt_processor($sec);
+    return("Problem extracting \'$name\' data ($err). ", $string)
+      if (! MMisc::is_blank($err));
   }
 
-  $xmllint =~ s{^~([^/]*)}{$1?(getpwnam($1))[7]:($ENV{HOME} || $ENV{LOGDIR})}ex;
+  return("", "");
+}
 
-  # Check that the file for xmllint exists and is an executable file
-  return("", "\'xmllint\' ($xmllint) does not exist, aborting\n")
-    if (! -e $xmllint);
+#####
 
-  return("", "\'xmllint\' ($xmllint) is not a file, aborting\n")
-    if (! -f $xmllint);
+sub _excerpt_processor {
+  my ($self, $string) = @_;
 
-  return("", "\'xmllint\' ($xmllint) is not executable, aborting\n")
-    if (! -x $xmllint);
+  my %tmp;
 
-  # Now check that it actually is xmllint
-  my ($retcode, $stdout, $stderr) = MMisc::do_system_call($xmllint, '--version');
-  return("", "\'xmllint\' ($xmllint) does not seem to be a valid \'xmllint\' command, aborting\n")
-    if ($retcode != 0);
+  # Remove the excerpt header/trailer tags
+  my $name = "excerpt";
+  return("Could not remvoe the \'$name\' tag, aborting", $string)
+      if (! MtXML::remove_xml_tags($name, \$string));  
+
+  foreach my $tag (@required_excerpt_tags) {
+    my ($err, $value) = &_get_named_xml_value($tag, \$string);
+    return("Could not find required \'excerpt\' tag ($tag), aborting", $value)
+      if (! MMisc::is_blank($err)); 
+    $tmp{$tag} = $value;
+  }
+
+  foreach my $tag (@optional_excerpt_tags) {
+    my ($err, $value) = &_get_named_xml_value($tag, \$string);
+    next if (! MMisc::is_blank($err)); 
+    $tmp{$tag} = $value;
+  }
+#  print "[*]", Dumper(\%tmp), "\n";
+
+  my %fhash;
+  %fhash = $self->_get_fhash() if ($self->_is_fhash_set());
   
-  if ($stderr =~ m%using\s+libxml\s+version\s+(\d+)%) {
-    # xmllint print the command name followed by the version number
-    my $version = $1;
-    return("", "\'xmllint\' ($xmllint) version too old: requires at least 2.6.30 (ie 20630, installed $version), aborting\n")
-      if ($version <= 20630);
-  } else {
-    return("", "Could not confirm that \'xmllint\' is valid, aborting\n");
+  my @tmpa;
+  foreach my $tag (@required_excerpt_tags) {
+    push @tmpa, $tmp{$tag};
   }
+  my ($fn, $beg_ts, $duration_ts) = @tmpa;
+  my $end_ts = $beg_ts + $duration_ts;
 
-  return($xmllint, "");
+  my @tmpa;
+  foreach my $tag (@optional_excerpt_tags) {
+    if (! exists $tmp{$tag}) {
+      push @tmpa, $default_error_value;
+    } else {
+      push @tmpa, $tmp{$tag};
+    }
+  }
+  my ($fps, $lang, $stype) = @tmpa;
+
+  $fps = $self->get_default_fps() if ($fps eq $default_error_value);
+
+  my $fs_tmp = new ViperFramespan();
+  $fs_tmp->set_fps($fps);
+  return("Problem working with the 'fps' (" . $fs_tmp->get_errormsg() . ")")
+    if ($fs_tmp->error());
+
+  my $beg = $fs_tmp->ts_to_frame($beg_ts);
+  my $end = $fs_tmp->ts_to_frame($end_ts);
+  return("Problem converting ts to frame (" . $fs_tmp->get_errormsg() . ")")
+    if ($fs_tmp->error());
+  
+  $fs_tmp->set_value_beg_end($beg, $end);
+  return("Problem setting a ViperFramespan (" . $fs_tmp->get_errormsg() . ")")
+    if ($fs_tmp->error());
+  
+  my $terr = "";
+  if (exists $fhash{$fn}) {
+    # Previous values set, confirm it is the same
+    my $k = $optional_excerpt_tags[0]; # fps
+    if (exists $fhash{$fn}{$k}) {
+      my $tv = $fhash{$fn}{$k};
+      $terr .= "A previous \'$k\' value existed ($tv) for this 'excerpt' (file: $fn), and the value differs from the new found value ($fps). "
+	if ($fps != $tv);
+    }
+      
+    $k = $optional_excerpt_tags[1]; # language
+    if ((exists $fhash{$fn}{$k}) && ($lang ne $default_error_value)) {
+      my $tv = $fhash{$fn}{$k};
+      $terr .= "A previous \'$k\' value existed ($tv) for this 'excerpt' (file: $fn), and the value differs from the new found value ($lang). "
+	if ($lang != $tv);
+    }
+
+    $k = $optional_excerpt_tags[2]; # source type
+    if ((exists $fhash{$fn}{$k}) && ($stype ne $default_error_value)) {
+      my $tv = $fhash{$fn}{$k};
+      $terr .= "A previous \'$k\' value existed ($tv) for this 'excerpt' (file: $fn), and the value differs from the new found value ($stype). "
+	if ($stype != $tv);
+    }
+  }
+  return($terr) if (! MMisc::is_blank($terr));
+
+  # Now simply fill the %fhash
+  my $k = $optional_excerpt_tags[0]; # fps
+  $fhash{$fn}{$k} = $fps;
+  $k = $optional_excerpt_tags[1]; # language
+  $fhash{$fn}{$k} = $lang if ($lang ne $default_error_value);
+  $k = $optional_excerpt_tags[2]; # source_type
+  $fhash{$fn}{$k} = $stype if ($stype ne $default_error_value);
+  push @{$fhash{$fn}{$key_fs}}, $fs_tmp; # the framespan itself
+  push @{$fhash{$fn}{$key_ots}}, "$beg_ts : $end_ts"; # the original ts
+
+  return("Problem adding the 'excerpt' information to the object")
+    if (! $self->_set_fhash(%fhash));
+
+  return("");
 }
 
 #####
 
-sub _check_xsdfiles {
-  my $xsdpath = shift @_;
-  my @xsdfiles = @_;
+sub _get_named_xml_value {
+  my ($name, $rstring) = @_;
 
-  $xsdpath =~ s{^~([^/]*)}{$1?(getpwnam($1))[7]:($ENV{HOME} || $ENV{LOGDIR})}ex;
-  $xsdpath =~ s%(.)\/$%$1%;
+  my $sec = MtXML::get_named_xml_section($name, $rstring, $default_error_value);
+  return("Could not find \'$name\' data", $$rstring)
+    if ($sec eq $default_error_value);
 
-  foreach my $fname (@xsdfiles) {
-    my $file = "$xsdpath/$fname";
-    return("", "Could not find required XSD file ($fname) at selected path ($xsdpath), aborting\n")
-      if (! -e $file);
+  return("Could not remvoe the \'$name\' tag, aborting", $$rstring)
+    if (! MtXML::remove_xml_tags($name, \$sec));  
+  
+  return("", $sec);
+}
+
+########## 'duration', 'version'
+
+sub _get_XXX {
+  my ($self, $XXX) = @_;
+
+  if (! $self->is_validated()) {
+    $self->_set_errormsg("Can only obtain this information from a validated ECF");
+    return(0);
   }
 
-  return($xsdpath, "");
+  return($self->{XXX});
+}
+
+#####
+
+sub get_duration {
+  my ($self) = @_;
+  return($self->_get_XXX("duration"));
+}
+
+#####
+
+sub get_version {
+  my ($self) = @_;
+  return($self->_get_XXX("version"));
+}
+
+########## 'fhash'
+
+sub _set_fhash {
+  my ($self, %fhash) = @_;
+
+  return(0) if ($self->error());
+
+  $self->{fhash} = \%fhash;
+  return(1);
+}
+
+#####
+
+sub _is_fhash_set {
+  my ($self) = @_;
+
+  return(0) if ($self->error());
+
+  return(1) if (defined $self->{fhash});
+
+  return(0);
+}
+
+#####
+
+sub _get_fhash {
+  my ($self) = @_;
+
+  return(-1) if ($self->error());
+
+  if (! $self->_is_fhash_set()) {
+    $self->_set_errormsg("\'fhash\' is not set");
+    return(0);
+  }
+
+  my $rfhash = $self->{fhash};
+
+  my %res = %{$rfhash};
+
+  return(%res);
+}
+
+####################
+
+sub _display {
+  my ($self) = @_;
+
+  print Dumper(\$self);
 }
 
 ############################################################
