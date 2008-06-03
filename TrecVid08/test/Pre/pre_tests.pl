d6ebff61adafdd7448da208f02ab395723860bbc
Martial Michel <martial.michel@nist.gov>
2008-06-03 22:45:12 +0000

    Full .pm check added

--- TrecVid08/test/Pre/pre_tests.pl
+++ TrecVid08/test/Pre/pre_tests.pl
@@ -8,20 +8,28 @@ my $err = 0;
 
 ##########
 print "** Checking for Perl Packages:\n";
-my @tocheck = ("Getopt::Long", "Data::Dumper", "File::Temp", 
-#	       "Statistics::Descriptive::Discrete", 
-	       );
-my $ms = scalar @tocheck;
-foreach my $i (@tocheck) {
-  print "- $i : ";
-  my $v = MMisc::check_package($i);
-  my $t = $v ? "ok" : "**missing**";
-  print "$t\n";
-  $ms -= $v;
-}
+my $ms;
+print "[F4DE Provided Packages]\n";
+$ms = &_chkpkg("BipartiteMatch", "DETCurve", "MMisc", "MtXML", "SimpleAutoTable", "Trials", "MErrorH", "MetricTestStub", "PropList", "TrialSummaryTable", "ViperFramespan", "MetricTV08","TrecVid08ECF", "TrecVid08EventList", "TrecVid08Observation", "TrecVid08ViperFile", "TrecVid08xmllint");
 if ($ms > 0) {
-  print "  ** Not all packages found, you will not be able to run the program, please install the missing ones\n\n";
+  print "  ** ERROR: Not all packages found, you will not be able to run the program, please install the missing ones\n\n";
   $err++;
+} else {
+  print "  Found all packages\n\n";
+}
+print "[Required Packages]\n";
+$ms = _chkpkg("Getopt::Long", "Data::Dumper", "File::Temp");
+if ($ms > 0) {
+  print "  ** ERROR: Not all packages found, you will not be able to run the program, please install the missing ones\n\n";
+  $err++;
+} else {
+  print "  Found all packages\n\n";
+}
+
+print "[Optional Packages]\n";
+$ms = &_chkpkg("Statistics::Descriptive::Discrete"); # TV08Scorer
+if ($ms > 0) {
+  print "  ** WARNING: Not all TV08Scorer packages found, you will not be able to run this program\n";
 } else {
   print "  Found all packages\n\n";
 }
@@ -43,6 +51,7 @@ if ($xobj->error()) {
   print "After installing a suitable version, set the $xmllint_env environment variable to ensure the use of the proper version if it is not in your PATH\n";
   $err++;
 } else {
+  $xmllint = $xobj->get_xmllint();
   print "  xmllint ($xmllint) is ok and recent enough\n";
 }
 
@@ -51,6 +60,24 @@ if ($xobj->error()) {
 error_quit("\nSome issues, fix before attempting to run make check again\n") if ($err);
 
 ok_quit("\n** Pre-requisite testing done\n\n");
+
+####################
+
+sub _chkpkg {
+  my @tocheck = @_;
+
+  my $ms = scalar @tocheck;
+  foreach my $i (@tocheck) {
+    print "- $i : ";
+    my $v = MMisc::check_package($i);
+    my $t = $v ? "ok" : "**missing**";
+    print "$t\n";
+    $ms -= $v;
+  }
+
+  return($ms);
+}
+
 
 ####################
 
