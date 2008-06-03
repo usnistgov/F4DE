7f6cd2486bf5ef347ef567b86b85416748cfe3ad
Martial Michel <martial.michel@nist.gov>
2008-06-03 16:09:59 +0000

    Initial Commit

---
+++ TrecVid08/tools/TV08MergeHelper/ECFValidator.pl
@@ -0,0 +1,277 @@
+#!/usr/bin/env perl
+
+# TrecVid08 ECF XML Validator
+#
+# Author(s): Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "TrecVid08 ECF XML Validator" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+use strict;
+
+# Note: Designed for UNIX style environments (ie use cygwin under Windows).
+
+##########
+# Version
+
+# $Id$
+
+my $version     = "0.1b";
+
+if ($version =~ m/b$/) {
+  (my $cvs_version = '$Revision$') =~ s/[^\d\.]//g;
+  $version = "$version (CVS: $cvs_version)";
+}
+
+my $versionid = "TrecVid08 ECF XML Validator Version: $version";
+
+##########
+# Check we have every module (perl wise)
+
+## First insure that we add the proper values to @INC
+my ($f4b, $f4bv, $tv08pl, $tv08plv, $f4depl, $f4deplv);
+BEGIN {
+  $f4b = "F4DE_BASE";
+  $f4bv = $ENV{$f4b} . "/lib";
+  $tv08pl = "TV08_PERL_LIB";
+  $tv08plv = $ENV{$tv08pl} || "../../lib"; # Default is relative to this tool's default path
+  $f4depl = "F4DE_PERL_LIB";
+  $f4deplv = $ENV{$f4depl} || "../../../common/lib";  # Default is relative to this tool's default path
+}
+use lib ($tv08plv, $f4deplv, $f4bv);
+
+sub eo2pe {
+  my @a = @_;
+  my $oe = join(" ", @a);
+  my $pe = ($oe !~ m%^Can\'t\s+locate%) ? "\n----- Original Error:\n $oe\n-----" : "";
+  return($pe);
+}
+
+## Then try to load everything
+my $ekw = "ERROR"; # Error Key Work
+my $have_everything = 1;
+my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable (if you did an install, otherwise your $tv08pl and $f4depl environment variables).";
+
+# MMisc (part of this tool)
+unless (eval "use MMisc; 1")
+  {
+    my $pe = &eo2pe($@);
+    warn_print("\"MMisc\" is not available in your Perl installation. ", $partofthistool, $pe);
+    $have_everything = 0;
+  }
+
+# TrecVid08ECF (part of this tool)
+unless (eval "use TrecVid08ECF; 1")
+  {
+    my $pe = &eo2pe($@);
+    warn_print("\"TrecVid08ECF\" is not available in your Perl installation. ", $partofthistool, $pe);
+    $have_everything = 0;
+  }
+
+# Getopt::Long (usualy part of the Perl Core)
+unless (eval "use Getopt::Long; 1")
+  {
+    warn_print
+      (
+       "\"Getopt::Long\" is not available on your Perl installation. ",
+       "Please see \"http://search.cpan.org/search?mode=module&query=getopt%3A%3Along\" for installation information\n"
+      );
+    $have_everything = 0;
+  }
+
+# Something missing ? Abort
+error_quit("Some Perl Modules are missing, aborting\n") unless $have_everything;
+
+# Use the long mode of Getopt
+Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
+
+########################################
+# Get some values from TrecVid08ECF
+my $dummy = new TrecVid08ECF();
+my @xsdfilesl = $dummy->get_required_xsd_files_list();
+# We will use the '$dummy' to do checks before processing files
+
+########################################
+# Options processing
+
+my $xmllint_env = "TV08_XMLLINT";
+my $xsdpath_env = "TV08_XSDPATH";
+my $usage = &set_usage();
+
+# Default values for variables
+my $xmllint = MMisc::get_env_val($xmllint_env, "");
+my $xsdpath = MMisc::get_env_val($xsdpath_env, "../../data");
+$xsdpath = "$f4bv/data" 
+  if (($f4bv ne "/lib") && ($xsdpath eq "../../data"));
+my $show = 0;
+my $fps = 0;
+
+# Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+# USed:                    T           f h             v x  
+
+my %opt;
+my $dbgftmp = "";
+GetOptions
+  (
+   \%opt,
+   'help',
+   'version',
+   'xmllint=s'       => \$xmllint,
+   'TrecVid08xsd=s'  => \$xsdpath,
+   'fps=i'           => \$fps,
+    # Hiden Option(s)
+   'show_internals'  => \$show,
+  ) or error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
+
+ok_quit("\n$usage\n") if ($opt{'help'});
+ok_quit("$versionid\n") if ($opt{'version'});
+
+ok_quit("\n$usage\n") if (scalar @ARGV == 0);
+
+if ($xmllint ne "") {
+  error_quit("While trying to set \'xmllint\' (" . $dummy->get_errormsg() . ")")
+    if (! $dummy->set_xmllint($xmllint));
+}
+
+if ($xsdpath ne "") {
+  error_quit("While trying to set \'TrecVid08xsd\' (" . $dummy->get_errormsg() . ")")
+    if (! $dummy->set_xsdpath($xsdpath));
+}
+
+##########
+# Main processing
+my $tmp;
+my %all = ();
+my $ntodo = scalar @ARGV;
+my $ndone = 0;
+while ($tmp = shift @ARGV) {
+  my ($ok, $object) = &load_file($tmp);
+  next if (! $ok);
+
+  $all{$tmp} = $object;
+  $ndone++;
+}
+ok_quit("All files processed (Validated: $ndone | Total: $ntodo)\n");
+
+########## END
+
+sub valok {
+  my ($fname, $txt) = @_;
+
+  print "$fname: $txt\n";
+}
+
+#####
+
+sub valerr {
+  my ($fname, $txt) = @_;
+  foreach (split(/\n/, $txt)){ 
+    &valok($fname, "[ERROR] $_");
+  }
+}
+
+##########
+
+sub load_file {
+  my ($tmp) = @_;
+
+  if (! -e $tmp) {
+    &valerr($tmp, "file does not exists, skipping");
+    return(0, ());
+  }
+  if (! -f $tmp) {
+    &valerr($tmp, "is not a file, skipping\n");
+    return(0, ());
+  }
+  if (! -r $tmp) {
+    &valerr($tmp, "file is not readable, skipping\n");
+    return(0, ());
+  }
+  
+  # Prepare the object
+  my $object = new TrecVid08ECF();
+  error_quit("While trying to set \'xmllint\' (" . $object->get_errormsg() . ")")
+    if ( ($xmllint ne "") && (! $object->set_xmllint($xmllint)) );
+  error_quit("While trying to set \'TrecVid08xsd\' (" . $object->get_errormsg() . ")")
+    if ( ($xsdpath ne "") && (! $object->set_xsdpath($xsdpath)) );
+  error_quit("While setting \'file\' ($tmp) (" . $object->get_errormsg() . ")")
+    if ( ! $object->set_file($tmp) );
+  error_quit("While setting \'fps\' ($fps) (" . $object->get_errormsg() . ")")
+    if ( ! $object->set_default_fps($fps) );
+
+  # Validate
+  if (! $object->validate()) {
+    &valerr($tmp, $object->get_errormsg());
+    return(0, ());
+  }
+
+  &valok($tmp, "validates");
+
+  # This is really if you are a debugger
+  print("** Memory Representation:\n", $object->_display()) if ($show);
+
+  return(1, $object);
+}
+
+########################################
+
+sub set_usage {
+  my $xsdfiles = join(" ", @xsdfilesl);
+  my $tmp=<<EOF
+$versionid
+
+Usage: $0 [--help] [--version] [--xmllint location] [--TrecVid08xsd location]  --fps fps ecf_source_file.xml [ecf_source_file.xml [...]]
+
+Will perform a semantic validation of the ECF XML file(s) provided.
+
+ Where:
+  --fps           Set the default Frame per Second value for files within the ECF
+  --xmllint       Full location of the \'xmllint\' executable (can be set using the $xmllint_env variable)
+  --TrecVid08xsd  Path where the XSD files can be found (can be set using the $xsdpath_env variable)
+  --version       Print version number and exit
+  --help          Print this usage information and exit
+
+Note:
+- This prerequisite that the file can be been validated using 'xmllint' against the XSD file
+- Program will disard any xml comment(s).
+- 'TrecVid08xsd' files are: $xsdfiles
+EOF
+;
+
+  return $tmp;
+}
+
+####################
+
+sub warn_print {
+  print "WARNING: ", @_;
+
+  print "\n";
+}
+
+##########
+
+sub error_quit {
+  print("${ekw}: ", @_);
+
+  print "\n";
+  exit(1);
+}
+
+##########
+
+sub ok_quit {
+  print @_;
+
+  print "\n";
+  exit(0);
+}
