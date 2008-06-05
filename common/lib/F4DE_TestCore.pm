09aea2a320b90b0538782b79476924e2d493a9ee
Martial Michel <martial.michel@nist.gov>
2008-06-05 19:51:40 +0000

    Added run_complextest

--- common/lib/F4DE_TestCore.pm
+++ common/lib/F4DE_TestCore.pm
@@ -1,8 +1,11 @@
 package TV08TestCore;
 
+use strict;
 use MMisc;
 
 my $magicmode = "makecheckfiles"; # And the magic word is ...
+my $dev = "TV08TestCore default error value";
+my $lts = 10; # lines to show
 
 #####
 
@@ -20,6 +23,18 @@ sub get_txt_last_Xlines {
   return(@toshowa);
 }
 
+##########
+
+sub _get_filec {
+  my $f = shift @_;
+
+  return($dev) if ((! -e $f) || (! -r $f));
+
+  my $txt = MMisc::slurp_file($f);
+
+  return($txt);
+}
+
 #####
 
 sub make_syscall {
@@ -29,22 +44,24 @@ sub make_syscall {
 
   open OFILE, ">$ofile"
     or die("TV08TestCore Internal Error: Could not create output file ($ofile) : $!\n");
-  print OFILE "[[RETURN CODE]] $retcode\n",
-    "[[STDOUT]]\n$stdout\n\n[[STDERR]]\n$stderr\n";
+  print OFILE "[[COMMANDLINE]] ", join(" ", @command), "\n"
+    , "[[RETURN CODE]] $retcode\n"
+      , "[[STDOUT]]\n$stdout\n\n"
+	, "[[STDERR]]\n$stderr\n";
   close OFILE;
  
-  my @toshow = &get_txt_last_Xlines($stdout, 10);
+  my @toshow = &get_txt_last_Xlines($stdout, $lts);
 
-  my $txt = MMisc::slurp_file($ofile);
+  my $txt = &_get_filec($ofile);
+  $retcode = 1 if ($txt eq $dev);
+
   return($retcode, $txt, @toshow);
 }
 
 ##########
 
-sub run_simpletest {
+sub _run_core {
   my ($testname, $cmd, $cmp2resfile, $mode) = @_;
-
-  print "$testname: ";
 
   my $ofile;
   if ($mode eq $magicmode) {
@@ -55,35 +72,154 @@ sub run_simpletest {
 
   my ($retcode, $run, @toshow) = &make_syscall($ofile, $cmd);
 
+  my $tst = join("\n  ## ", @toshow);
+
+  return(0, $ofile, $tst) if ($retcode != 0);
+
+  return(1, $ofile, $tst) if ($mode eq $magicmode);
+
+  my $runfile = &_get_filec($ofile);
+  my $resfile = &_get_filec($cmp2resfile);
+  unlink($ofile);
+
+  return(0) if (($runfile eq $dev) || ($resfile eq $dev));
+
+  return(1, $ofile, $tst) if ($runfile eq $resfile);
+
+  return(0, $ofile, $tst);
+}
+
+##########
+  
+sub run_simpletest {
+  my ($testname, $cmd, $cmp2resfile, $mode) = @_;
+
+  print "$testname: ";
+
+  my ($rv, $ofile, $toshow) = &_run_core(@_);
+
   if ($mode eq $magicmode) {
     print "makecheckfile ... ";
-    if ($retcode != 0) {
+    if ($rv == 0) {
       print "##### failed ! ##### (see $ofile)\n";
-      print "## Last 10 lines of stdout run:\n  ## ", join("\n  ## ", @toshow), "\n";
+      print "## Last $lts lines of stdout run:\n  ## $toshow\n";
       return(0);
     }
     print "ok [wrote: $ofile]\n";
-    print "## Last 10 lines of stdout run:\n  ## ", join("\n  ## ", @toshow), "\n";
+    print "## Last $lts lines of stdout run:\n  ## $toshow\n";
     return(1);
   }
 
-  if ($retcode != 0) {
+  if ($rv == 0) {
     print " failed\n";
     return(0);
   }
 
-  my $runfile = MMisc::slurp_file($ofile);
-  my $resfile = MMisc::slurp_file($cmp2resfile);
-  unlink($ofile);
+  print "OK\n";
+  return(1);
+}
 
-  if ($runfile eq $resfile) {
-    print "OK\n";
-    # Erase the temporary file
+####################
+
+sub _split_fn {
+  my $v = shift @_;
+  
+  my ($sf, $df) = split(m%\:%, $v);
+  
+  return($sf,$df);
+}
+
+#####
+
+sub _get_sfdf_core {
+  my ($v, $p) = @_;
+  
+  my ($sf, $df) = &_split_fn($v);
+  return($dev) if ((MMisc::is_blank($sf)) || (MMisc::is_blank($df)));
+  
+  my @it = ($sf, $df);
+  my $fc = &_get_filec($it[$p]);
+
+  return($fc);
+}
+
+#####
+
+sub _get_sfc {
+  my ($v) = @_;
+  return(&_get_sfdf_core($v, 0));
+}
+
+#####
+  
+sub _get_dfc {
+  my ($v) = @_;
+  return(&_get_sfdf_core($v, 1));
+}
+
+#####
+
+sub _mcfgetf {
+  my @vs = @_;
+  
+  foreach my $v (@vs) {
+    my ($sf, $df) = &_split_fn($v);
+    my $sfc = &_get_sfc($v);
+    return(0) if ($sfc eq $dev);
+    open FILE, ">$df"
+      or die("TV08TestCore Internal Error: Could not create output file ($df) : $!\n");
+    print FILE $sfc;
+    close FILE;
+  }
+
+  return(1);
+}
+ 
+#####
+
+sub _cmp_sfc_dfc {
+  my @vs = @_;
+
+  foreach my $v (@vs) {
+    my $sfc = &_get_sfc($v);
+    return(0) if ($sfc eq $dev);
+    my $dfc = &_get_dfc($v);
+    return(0) if ($dfc eq $dev);
+    
+    return(0) if ($sfc ne $dfc);
+  }
+
+  return(1);
+}
+
+#####
+
+sub run_complextest {
+  my ($testname, $cmd, $cmp2resfile, $mode, @sfiles) = @_;
+
+  print "$testname: ";
+
+  my ($rv, $ofile, $toshow) = &_run_core(@_);
+
+  if ($mode eq $magicmode) {
+    print "makecheckfile ... ";
+    if (($rv == 0) || (! &_mcfgetf(@sfiles))) {
+      print "##### failed ! ##### (see $ofile)\n";
+      print "## Last $lts lines of stdout run:\n  ## $toshow\n";
+      return(0);
+    }
+    print "ok [wrote: $ofile]\n";
+    print "## Last $lts lines of stdout run:\n  ## $toshow\n";
     return(1);
   }
-  
-  print "failed\n";
-  return(0);
+
+  if (($rv == 0) || (! &_cmp_sfc_dfc(@sfiles))) {
+    print " failed\n";
+    return(0);
+  }
+
+  print "OK\n";
+  return(1);
 }
 
 ########################################
