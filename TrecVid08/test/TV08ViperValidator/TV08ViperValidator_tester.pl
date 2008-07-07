05d94c184f9cc9ee0b258f998eab956f40a6b62d
Martial Michel <martial.michel@nist.gov>
2008-07-07 20:52:00 +0000

    Integrated 'subEventtypes' tests

--- TrecVid08/test/TV08ViperValidator/TV08ViperValidator_tester.pl
+++ TrecVid08/test/TV08ViperValidator/TV08ViperValidator_tester.pl
@@ -14,19 +14,21 @@ my $totest = 0;
 my $testr = 0;
 
 $testr += &do_test1("Test 0 (Base XML Generation)", "-X", "res_test0.txt");
-$totest++;
 
 $testr += &do_test1("Test 1 (Not a Viper File)", "TV08ViperValidator_tester.pl", "res_test1.txt");
-$totest++;
 
 $testr += &do_test1("Test 2 (GTF files check)", "../../test/common/test1-gtf.xml ../../test/common/test2-gtf.xml -g -w", "res_test2.txt");
-$totest++;
 
 $testr += &do_test1("Test 3 (SYS file check)", "../../test/common/test1-1fa-sys.xml ../../test/common/test1-1md-sys.xml ../../test/common/test1-same-sys.xml ../../test/common/test2-1md_1fa-sys.xml ../../test/common/test2-same-sys.xml -w", "res_test3.txt");
-$totest++;
 
 $testr += &do_test1("Test 4 (limitto check)", "../../test/common/test1-gtf.xml ../../test/common/test2-gtf.xml -g -w -l ObjectPut", "res_test4.txt");
-$totest++;
+
+$testr += &do_test1("Test 5a (subEventtypes)", "../../test/common/test5-subEventtypes-sys.xml -w", "res_test5a.txt");
+
+$testr += &do_test1("Test 5b (subEventtypes + pruneEvents)", "../../test/common/test5-subEventtypes-sys.xml -w -p", "res_test5b.txt");
+
+$testr += &do_test1("Test 5c (subEventtypes + pruneEvents + removeSubEventtypes)", "../../test/common/test5-subEventtypes-sys.xml -w -p -r", "res_test5c.txt");
+
 
 if ($testr == $totest) {
   ok_quit("All tests ok\n\n");
@@ -42,6 +44,7 @@ sub do_test1 {
   my ($testname, $args, $res) = @_;
 
   my $command = "$validator $args";
+  $totest++;
 
   return(TV08TestCore::run_simpletest($testname, $command, $res, $mode));
 }
