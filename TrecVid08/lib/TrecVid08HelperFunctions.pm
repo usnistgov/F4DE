a3f2f18da08a6e7f0407c631c267bf03095188c5
Martial Michel <martial.michel@nist.gov>
2008-07-14 20:19:41 +0000

    Intial commit. include 'ViperFile_crop' function.

---
+++ TrecVid08/lib/TrecVid08HelperFunctions.pm
@@ -0,0 +1,131 @@
+package TrecVid08HelperFunctions;
+
+# TrecVid08 HelperFunctions
+#
+# Author(s): Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "TrecVid08HelperFunctions.pm" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+
+# $Id$
+
+use strict;
+use ViperFramespan;
+use TrecVid08ViperFile;
+use TrecVid08Observation;
+
+use MErrorH;
+use MMisc;
+
+use Data::Dumper;
+
+my $version     = "0.1b";
+
+if ($version =~ m/b$/) {
+  (my $cvs_version = '$Revision$') =~ s/[^\d\.]//g;
+  $version = "$version (CVS: $cvs_version)";
+}
+
+my $versionid = "TrecVid08HelperFunctions.pm Version: $version";
+
+####################
+
+sub ViperFile_crop {
+  my ($vf, $beg, $end) = @_;
+
+  return("ViperFile is not validated, aborting", undef)
+    if (! $vf->is_validated());
+  
+  return("End frame before Beginning frame, aborting", undef)
+    if ($beg > $end);
+  
+  return("Beginning frame less than 1, aborting", undef)
+    if ($beg < 1);
+
+  my $onf = $vf->get_numframes_value();
+  return("Problem obtaining the ViperFile NUMFRAMES value (" . $vf->get_errormsg() . ")", undef)
+    if ($vf->error());
+
+  return("Requested end goes past the File's NUMFRAMES, can only crop known values", undef)
+    if ($end > $onf);
+
+  ## Cropping is a 2 steps operations:
+  # - trim to select values
+  # - shifting all elements to the beginning of the 
+
+  # Get an observation representation of all the viper file
+  my @ao = $vf->get_all_events_observations();
+  return("Problem while obtaining Observations (" . $vf->get_errormsg() . ")", undef)
+    if ($vf->error());
+
+  # We need a ViperFramespan to work overlap
+  my $fps = $vf->get_fps();
+  return("Problem obtaining the ViperFile's fps (" . $vf->get_errormsg() . ")", undef)
+    if ($vf->error());
+  my $fs_range = new ViperFramespan();
+  $fs_range->set_value_beg_end($beg, $end);
+  $fs_range->set_fps($fps);
+  return("Problem creating a ViperFramespan (" . $fs_range->get_errormsg() . ")", undef)
+    if ($fs_range->error());
+  my $fsr_txt = $fs_range->get_value();
+  return("Problem obtaining the ViperFramespan's value (" . $fs_range->get_errormsg() . ")", undef)
+    if ($fs_range->error());
+
+  # Crop each observation in turn
+  my @left = ();
+  foreach my $obs (@ao) {
+    # Check if the observation is even overlapping with the area
+    my $fs_ov = $obs->get_framespan_overlap_from_fs($fs_range);
+    return("Problem computing the range overlap (" . $obs->get_errormsg() . ")", undef)
+      if ($obs->error());
+    next if (! defined $fs_ov);
+
+    # Now, crop to the fs_range
+    my $crop_done = $obs->crop_to_fs($fs_range);
+    return("Problem cropping observation to selected range (" . $obs->get_errormsg() . ")", undef)
+      if ($obs->error());
+    next if (! $crop_done);
+
+    push @left, $obs;
+  }
+
+  # Now we simply shift by (1 - beg)
+  # ex: new beg is 2000 we shift by -1999 to start at 1
+  my $fsshift = 1 - $beg;
+  if ($fsshift != 0) {
+    foreach my $obs (@left) {
+      $obs->shift_framespan($fsshift);
+      return("Problem shifting observation (" . $obs->get_errormsg() . ")", undef)
+	if ($obs->error());
+    }
+  }
+
+  # Prepare the output ViperFile
+  my $vfc = $vf->clone_with_no_events();
+
+  # Next step is to adapt the ViperFile's NUMFRAMES to its new boundaries
+  my $nf = $end + $fsshift; # fsshift is negative
+  my $an = $vfc->modify_numframes($nf, "Cropped to [$fsr_txt], then shifted by $fsshift to have the first frame start at 1");
+  return("Problem modifying numframes (" . $vfc->get_errormsg() . ")", undef)
+    if ($vfc->error());
+
+  # And finaly to add the observations to the newly created file
+  foreach my $obs (@left) {
+    $vfc->add_observation($obs, 1);
+    return("Problem adding cropped observations to viper file (" . $vfc->get_errormsg() . ")", undef)
+      if ($vfc->error());
+  }
+
+  return("", $vfc);
+}
+
