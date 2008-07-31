c4b26901a7090ae8d88d2024e09307a02bc6084d
Jonathan Fiscus <jonathan.fiscus@nist.gov>
2008-07-31 16:54:31 +0000

    Added a unit test. methods to access data within the objects.  Added file system safe key that can be used in a filename

--- common/lib/DETCurveSet.pm
+++ common/lib/DETCurveSet.pm
@@ -19,6 +19,9 @@ package DETCurveSet;
 
 use strict;
 use Data::Dumper;
+use Trials;
+use MetricTestStub;
+use DETCurve;
 
 sub new
   {
@@ -28,27 +31,161 @@ sub new
       { 
        Title => $title,
        DETList => [ () ], ### an array of hashes containing the det curves,  
+       KEYLUT => {}
       };
 
     bless $self;
     return $self;
   }
 
+sub unitTest(){
+  print "Test DETCurveSet\n";
+
+  my @isolinecoef = ( 5, 10, 20, 40, 80, 160 );
+  my $trial = new Trials("Term Detection", "Term", "Occurrence", { ("TOTALTRIALS" => undef) });
+    
+  $trial->addTrial("she", 0.10, "NO", 0);
+  $trial->addTrial("she", 0.15, "NO", 0);
+  $trial->addTrial("she", 0.20, "NO", 0);
+  $trial->addTrial("she", 0.25, "NO", 0);
+  $trial->addTrial("she", 0.30, "NO", 1);
+  $trial->addTrial("she", 0.35, "NO", 0);
+  $trial->addTrial("she", 0.40, "NO", 0);
+  $trial->addTrial("she", 0.45, "NO", 1);
+  $trial->addTrial("she", 0.50, "NO", 0);
+  $trial->addTrial("she", 0.55, "YES", 1);
+  $trial->addTrial("she", 0.60, "YES", 1);
+  $trial->addTrial("she", 0.65, "YES", 0);
+  $trial->addTrial("she", 0.70, "YES", 1);
+  $trial->addTrial("she", 0.75, "YES", 1);
+  $trial->addTrial("she", 0.80, "YES", 1);
+  $trial->addTrial("she", 0.85, "YES", 1);
+  $trial->addTrial("she", 0.90, "YES", 1);
+  $trial->addTrial("she", 0.95, "YES", 1);
+  $trial->addTrial("she", 1.0, "YES", 1);
+
+  my $trial2 = new Trials("Term Detection", "Term", "Occurrence", { ("TOTALTRIALS" => undef) });
+    
+  $trial2->addTrial("she", 0.10, "NO", 0);
+  $trial2->addTrial("she", 0.15, "NO", 0);
+  $trial2->addTrial("she", 0.20, "NO", 0);
+  $trial2->addTrial("she", 0.25, "NO", 0);
+  $trial2->addTrial("she", 0.30, "NO", 1);
+  $trial2->addTrial("she", 0.35, "NO", 1);
+  $trial2->addTrial("she", 0.40, "NO", 0);
+  $trial2->addTrial("she", 0.45, "NO", 1);
+  $trial2->addTrial("she", 0.50, "NO", 0);
+  $trial2->addTrial("she", 0.55, "YES", 1);
+  $trial2->addTrial("she", 0.60, "YES", 1);
+  $trial2->addTrial("she", 0.65, "YES", 0);
+  $trial2->addTrial("she", 0.70, "YES", 0);
+  $trial2->addTrial("she", 0.75, "YES", 1);
+  $trial2->addTrial("she", 0.80, "YES", 0);
+  $trial2->addTrial("she", 0.85, "YES", 1);
+  $trial2->addTrial("she", 0.90, "YES", 1);
+  $trial2->addTrial("she", 0.95, "YES", 1);
+  $trial2->addTrial("she", 1.0, "YES", 1);
+
+  my $det1 = new DETCurve($trial, 
+                          new MetricTestStub({ ('ValueC' => 0.1, 'ValueV' => 1, 'ProbOfTerm' => 0.0001 ) }, $trial),
+                          "pooled", "DET1", \@isolinecoef, undef);
+  my $det2 = new DETCurve($trial2, 
+                          new MetricTestStub({ ('ValueC' => 0.1, 'ValueV' => 1, 'ProbOfTerm' => 0.0001 ) }, $trial2),
+                          "pooled", "DET2", \@isolinecoef, undef);
+    
+  print " Added DETs... ";
+  my $ds = new DETCurveSet("title");
+  die "Error: Failed to add first det" if ("success" ne $ds->addDET("Name 1", $det1));
+  die "Error: Successful add of duplicate name" if ("success" eq $ds->addDET("Name 1", $det2));
+  my $exp = "Name______________________";
+  my $k2 = "Name !@#\$%^&*(){}[]?'\"\<\>:;";
+  die "Error: Failed to add second det" if ("success" ne $ds->addDET($k2, $det2));
+  print "OK\n";
+  
+  print " Check File System Safe keys... ";
+  die "Error: Filesystem safe name by key /".$ds->getFSKeyForKey("Name 1")."/ != /Name_1/" if ($ds->getFSKeyForKey("Name 1") ne "Name_1");
+  die "Error: Filesystem safe name by key /".$ds->getFSKeyForKey($k2)."/ != /$exp/" if ($ds->getFSKeyForKey($k2) ne $exp);
+  die "Error: Filesystem safe name by id /".$ds->getFSKeyForID(0)."/ != /Name_1/" if ($ds->getFSKeyForID(0) ne "Name_1");
+  die "Error: Filesystem safe name by id /".$ds->getFSKeyForID(1)."/ != /$exp/" if ($ds->getFSKeyForID(1) ne $exp);
+  
+#  DETCurve::writeMultiDetGraph("foomerge", [($det1, $det2)]);
+#  print DETCurve::writeMultiDetSummary([($det1, $det2)], "text");
+
+
+  print "OK\n";
+} 
+
+
 sub addDET(){
   my ($self, $name, $det) = @_;
     
   return "Name not defined" unless defined $name;
   return "DET not defined" unless defined $det;
-    
-  push @{ $self->{DETList} }, { KEY => $name, DET => $det };
-  if (@{ $self->{DETList} } > 1) {
-    ### Check to make sure the Metrics are all the same in the DETS
-    for (my $d=0; $d<@{ $self->{DETList} }; $d++) {
-      return "Error: DET[0] and DET[$d] are non-compatible objects" 
-        if (! $self->{DETList}->[0]->{DET}->isCompatible($self->{DETList}->[$d]->{DET}));
-    }   
+
+  #check that the name is uniq
+  return "Error: DET curve with name /$name/ already exists" if (exists($self->{KEYLUT}{$name}));
+  #Check that the filesystem-safe name is uniq
+  my $fskey = $name;
+  $fskey =~ s/[\s\/!\@\#\$\%\^\&\*\(\)\[\]\{\}\'\"\?\:\;\<\>]/_/g;
+  # Loop through the DET list
+  my $try = 1;
+  my $suffix = "";
+  my $done = 0;
+  while (! $done){
+    $done = 1;
+    foreach (@{ $self->{DETList} }){
+      if ($fskey.$suffix eq $_->{FILESYSSafeKey}){
+        $suffix = "_$try";
+        $try ++;
+        $done = 0; 
+      }
+    }
   }
+  $fskey .= $suffix;
+
+  ### Check to make sure the Metrics are all the same in the DETS
+  for (my $d=0; $d<@{ $self->{DETList} }; $d++) {
+    return "Error: the new det and DET[$d] are non-compatible objects" 
+      if (! $det->isCompatible($self->{DETList}->[$d]->{DET}));
+  }
+
+  push @{ $self->{DETList} }, { KEY => $name, DET => $det, FSSafeKey => $fskey};
+  $self->{KEYLUT}{$name} = $#{ $self->{DETList} };
   return "success";
+}
+
+sub getDETForKey(){
+  my ($self, $key) = @_;
+  
+  return undef if (! exists($self->{KEYLUT}{$key}));
+  return $self->{DETList}->[$self->{KEYLUT}{$key}]->{DET};
+}
+
+sub getDETForID(){
+  my ($self, $id) = @_;
+  
+  return undef if (@{ $self->{DETList} } < $id);
+  return $self->{DETList}->[$id]->{DET};
+}
+
+sub getFSKeyForID(){
+  my ($self, $id) = @_;
+  
+  return undef if (@{ $self->{DETList} } < $id);
+  return $self->{DETList}->[$id]->{FSSafeKey};
+}
+
+sub getFSKeyForKey(){
+  my ($self, $key) = @_;
+  
+  return undef if (! exists($self->{KEYLUT}{$key}));
+  return $self->{DETList}->[$self->{KEYLUT}{$key}]->{FSSafeKey};
+}
+
+sub getDETList(){
+  my ($self, $key) = @_;
+  
+  return $self->{DETList};
 }
 
 sub _PN(){
@@ -132,11 +269,7 @@ sub renderAsTxt(){
   ### Biuld the combined and separate DET PNGs
   my $multiInfo = {()};
   if ($buildCurves) {
-    my @DETs = ();
-    foreach my $item (@{ $self->{DETList} }) {
-      push @DETs, $item->{DET} if ($item->{DET}->successful);
-    }  
-    $multiInfo = DETCurve::writeMultiDetGraph($fileRoot, \@DETs, $DETOptions);
+    $multiInfo = DETCurve::writeMultiDetGraph($fileRoot, $self, $DETOptions);
   }
     
   my $at = $self->_buildAutoTable($buildCurves, $includeCounts, $reportActual);
