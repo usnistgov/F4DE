cd2317491e1a9b88f9b098e8e7ed05ac36020402
Martial Michel <martial.michel@nist.gov>
2008-08-13 18:42:05 +0000

    - Made $validator part of each call - Added test 8c (Memdump gzip save + load)

--- TrecVid08/test/TV08ViperValidator/TV08ViperValidator_tester.pl
+++ TrecVid08/test/TV08ViperValidator/TV08ViperValidator_tester.pl
@@ -17,78 +17,81 @@ my $tn = ""; # Test name
 
 ##
 $tn = "test0";
-$testr += &do_simple_test($tn, "(Base XML Generation)", "-X", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(Base XML Generation)", "$validator -X", "res_$tn.txt");
 
 ##
 $tn = "test1";
-$testr += &do_simple_test($tn, "(Not a Viper File)", "TV08ViperValidator_tester.pl", "res_${tn}.txt");
+$testr += &do_simple_test($tn, "(Not a Viper File)", "$validator TV08ViperValidator_tester.pl", "res_${tn}.txt");
 
 ##
 $tn = "test2";
-$testr += &do_simple_test($tn, "(GTF files check)", "../common/test1-gtf.xml ../common/test2-gtf.xml -g -w", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(GTF files check)", "$validator ../common/test1-gtf.xml ../common/test2-gtf.xml -g -w", "res_$tn.txt");
 
 ##
 $tn = "test3";
-$testr += &do_simple_test($tn, "(SYS file check)", "../common/test1-1fa-sys.xml ../common/test1-1md-sys.xml ../common/test1-same-sys.xml ../common/test2-1md_1fa-sys.xml ../common/test2-same-sys.xml -w", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(SYS file check)", "$validator ../common/test1-1fa-sys.xml ../common/test1-1md-sys.xml ../common/test1-same-sys.xml ../common/test2-1md_1fa-sys.xml ../common/test2-same-sys.xml -w", "res_$tn.txt");
 
 ##
 $tn = "test4";
-$testr += &do_simple_test($tn, "(limitto check)", "../common/test1-gtf.xml ../common/test2-gtf.xml -g -w -l ObjectPut", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(limitto check)", "$validator ../common/test1-gtf.xml ../common/test2-gtf.xml -g -w -l ObjectPut", "res_$tn.txt");
 
 ##
 $tn = "test5a";
-$testr += &do_simple_test($tn, "(subEventtypes)", "../common/test5-subEventtypes-sys.xml -w", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(subEventtypes)", "$validator ../common/test5-subEventtypes-sys.xml -w", "res_$tn.txt");
 
 $tn = "test5b";
-$testr += &do_simple_test($tn, "(subEventtypes + pruneEvents)", "../common/test5-subEventtypes-sys.xml -w -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(subEventtypes + pruneEvents)", "$validator ../common/test5-subEventtypes-sys.xml -w -p", "res_$tn.txt");
 
 $tn = "test5c";
-$testr += &do_simple_test($tn, "(subEventtypes + pruneEvents + removeSubEventtypes)", "../common/test5-subEventtypes-sys.xml -w -p -r", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(subEventtypes + pruneEvents + removeSubEventtypes)", "$validator ../common/test5-subEventtypes-sys.xml -w -p -r", "res_$tn.txt");
 
 ##
 $tn = "test6";
-$testr += &do_simple_test($tn, "(crop)", "../common/test1-1fa-sys.xml -w -p -c 1118:2000 -f 25", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(crop)", "$validator ../common/test1-1fa-sys.xml -w -p -c 1118:2000 -f 25", "res_$tn.txt");
 
 ##
 $tn = "test7a";
-$testr += &do_simple_test($tn, "(ChangeType SYS -> REF)", "../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -C 256 -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(ChangeType SYS -> REF)", "$validator ../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -C 256 -p", "res_$tn.txt");
 
 $tn = "test7b";
-$testr += &do_simple_test($tn, "(ChangeType REF -> SYS w/ randomseed)", "../common/test1-gtf.xml ../common/test2-gtf.xml -g -w -C 256 -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(ChangeType REF -> SYS w/ randomseed)", "$validator ../common/test1-gtf.xml ../common/test2-gtf.xml -g -w -C 256 -p", "res_$tn.txt");
 
 $tn = "test7c";
-$testr += &do_simple_test($tn, "(ChangeType REF -> SYS w/ randomseed + find_value)", "../common/test2-gtf.xml -g -w -C 256:0.120257329 -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(ChangeType REF -> SYS w/ randomseed + find_value)", "$validator ../common/test2-gtf.xml -g -w -C 256:0.120257329 -p", "res_$tn.txt");
 
 ##
 $tn = "test8a";
-$testr += &do_simple_test($tn, "(MemDump)", "../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -W text -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(MemDump)", "$validator ../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -W text -p", "res_$tn.txt");
 
 $tn = "test8b";
-$testr += &do_simple_test($tn, "(MemDump Load)", "../common/test1-1fa-sys.xml.memdump ../common/test2-1md_1fa-sys.xml.memdump -w -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(MemDump Load)", "$validator ../common/test1-1fa-sys.xml.memdump ../common/test2-1md_1fa-sys.xml.memdump -w -p", "res_$tn.txt");
+
+$tn = "test8c";
+$testr += &do_simple_test($tn, "(MemDump gzip save -> load)", "$validator ../common/test1-1fa-sys.xml -w . -W gzip && $validator test1-1fa-sys.xml.memdump -w -p; rm -f test1-1fa-sys.xml*", "res_$tn.txt");
 
 ##
 $tn = "test9";
-$testr += &do_simple_test($tn, "(ForceFilename)", "../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -p -F newfilename", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(ForceFilename)", "$validator ../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml -w -p -F newfilename", "res_$tn.txt");
 
 ##
 $tn = "test10a";
-$testr += &do_simple_test($tn, "(SYS: displaySummary)", "../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml ../common/test5-subEventtypes-sys.xml -d 3", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(SYS: displaySummary)", "$validator ../common/test1-1fa-sys.xml ../common/test2-1md_1fa-sys.xml ../common/test5-subEventtypes-sys.xml -d 3", "res_$tn.txt");
 
 $tn = "test10b";
-$testr += &do_simple_test($tn, "(REF: displaySummary)", "-g ../common/test1-gtf.xml ../common/test2-gtf.xml -d 3", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(REF: displaySummary)", "$validator -g ../common/test1-gtf.xml ../common/test2-gtf.xml -d 3", "res_$tn.txt");
 
 ##
 $tn = "test11a";
-$testr += &do_simple_test($tn, "(REF: ECF)", "-g ../common/test1-gtf.xml ../common/test2-gtf.xml -e ../common/tests.ecf -f NTSC -d 6", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(REF: ECF)", "$validator -g ../common/test1-gtf.xml ../common/test2-gtf.xml -e ../common/tests.ecf -f NTSC -d 6", "res_$tn.txt");
 
 $tn = "test11b";
-$testr += &do_simple_test($tn, "(REF: ECF + ChangeType + Crop)", "-g ../common/test1-gtf.xml ../common/test2-gtf.xml -e ../common/tests.ecf -f NTSC -d 6 -C word -w -p -c 20:1080", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(REF: ECF + ChangeType + Crop)", "$validator -g ../common/test1-gtf.xml ../common/test2-gtf.xml -e ../common/tests.ecf -f NTSC -d 6 -C word -w -p -c 20:1080", "res_$tn.txt");
 
 $tn = "test12a";
-$testr += &do_simple_test($tn, "(addXtraAttribute)", "../common/test1-1md-sys.xml -a addedattr:testvalue -a another_addedattr:value11 -w -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(addXtraAttribute)", "$validator ../common/test1-1md-sys.xml -a addedattr:testvalue -a another_addedattr:value11 -w -p", "res_$tn.txt");
 
 $tn = "test12b";
-$testr += &do_simple_test($tn, "(addXtraAttribute + crop)", "../common/test1-1md-sys.xml -a attr_name:person_11 -c 1450:1750 -f PAL -w -p", "res_$tn.txt");
+$testr += &do_simple_test($tn, "(addXtraAttribute + crop)", "$validator ../common/test1-1md-sys.xml -a attr_name:person_11 -c 1450:1750 -f PAL -w -p", "res_$tn.txt");
 
 ##########
 
@@ -103,9 +106,8 @@ die("You should never see this :)");
 ##########
 
 sub do_simple_test {
-  my ($testname, $subtype, $args, $res) = @_;
+  my ($testname, $subtype, $command, $res) = @_;
 
-  my $command = "$validator $args";
   $totest++;
 
   return(F4DE_TestCore::run_simpletest($testname, $subtype, $command, $res, $mode));
