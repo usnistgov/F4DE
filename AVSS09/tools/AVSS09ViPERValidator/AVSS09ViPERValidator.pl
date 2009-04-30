ac6b8796dcc9dc1c21e7e722bd443c452a62a6e6
Martial Michel <martial.michel@nist.gov>
2009-04-30 15:17:47 +0000

    Added 'man' entry

--- AVSS09/tools/AVSS09ViPERValidator/AVSS09ViPERValidator.pl
+++ AVSS09/tools/AVSS09ViPERValidator/AVSS09ViPERValidator.pl
@@ -98,7 +98,9 @@ Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
 
 my $xmllint_env = "CLEAR_XMLLINT";
 my $xsdpath_env = "CLEAR_XSDPATH";
+my $mancmd = "perldoc -F $0";
 my @ok_md = ("gzip", "text"); # Default is gzip / order is important
+my $frameTol = 0;
 my $usage = &set_usage();
 
 # Default values for variables
@@ -107,12 +109,11 @@ my $xmllint = MMisc::get_env_val($xmllint_env, "");
 my $xsdpath = MMisc::get_env_val($xsdpath_env, "../../../CLEAR07/data");
 $xsdpath = "$f4bv/data" 
   if (($f4bv ne "/lib") && ($xsdpath eq "../../../CLEAR07/data"));
-my $frameTol = 0;
 my $show = 0;
 my $forceFilename = "";
 my $writeback = -1;
 my $MemDump = undef;
-my $skipSequenceMemDump = 0;
+my $skipScoringSequenceMemDump = 0;
 
 # Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz  #
 # Used:   C  F                W        fgh          s  vwx    #
@@ -123,6 +124,7 @@ GetOptions
    \%opt,
    'help',
    'version',
+   'man',
    'xmllint=s'       => \$xmllint,
    'CLEARxsd=s'      => \$xsdpath,
    'gtf'             => \$isgtf,
@@ -130,13 +132,18 @@ GetOptions
    'ForceFilename=s' => \$forceFilename,
    'write:s'         => \$writeback,
    'WriteMemDump:s'  => \$MemDump,
-   'skipSequenceMemDump' => \$skipSequenceMemDump,
+   'skipScoringSequenceMemDump' => \$skipScoringSequenceMemDump,
    # Hiden Option(s)
    'X_show_internals'  => \$show,
   ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
 
-die("\n$usage\n") if ($opt{'help'});
-die("$versionid\n") if ($opt{'version'});
+MMisc::ok_quit("\n$usage\n") if ($opt{'help'});
+MMisc::ok_quit("$versionid\n") if ($opt{'version'});
+if ($opt{'man'}) {
+  my ($r, $o, $e) = MMisc::do_system_call($mancmd);
+  MMisc::error_quit("Could not run \'$mancmd\'") if ($r);
+  MMisc::ok_quit($o);
+}
 
 MMisc::error_quit("Not enough arguments\n$usage\n") if (scalar @ARGV == 0);
 
@@ -159,8 +166,8 @@ if (defined $MemDump) {
     if (! grep(m%^$MemDump$%, @ok_md));
 }
 
-MMisc::error_quit("\'skipSequenceMemDump\' can only be used if \'MemDump\' is selected")
-if (($skipSequenceMemDump) && (! defined $MemDump));
+MMisc::error_quit("\'skipScoringSequenceMemDump\' can only be used if \'MemDump\' is selected")
+if (($skipScoringSequenceMemDump) && (! defined $MemDump));
   
 ##############################
 # Main processing
@@ -185,6 +192,8 @@ foreach my $tmp (@ARGV) {
   MMisc::error_quit("Problem during \"transformations\": " . $object->get_errormsg())
       if ($object->error());
 
+  ## DCx test code would go here...
+
   if ($mods && $show) {
     print "** [After Mods]\n";
     print $object->_display_all();
@@ -204,7 +213,7 @@ foreach my $tmp (@ARGV) {
       if (! MMisc::writeTo($lfname, "", 1, 0, $txt, "", "** XML re-Representation:\n"));
 
     if (defined $MemDump) {
-      $object->write_MemDumps($lfname, $isgtf, $MemDump, $skipSequenceMemDump);
+      $object->write_MemDumps($lfname, $isgtf, $MemDump, $skipScoringSequenceMemDump);
       MMisc::error_quit("Problem while trying to perform \'MemDump\'")
           if ($object->error());
     }
@@ -258,6 +267,186 @@ sub _warn_add {
   $warn_msg .= "[Warning] " . join(" ", @_) . "\n";
 }
 
+############################################################ Manual
+
+=pod
+
+=head1 NAME
+
+AVSS09ViperValidator - AVSS09 ViPER XML Validator
+
+=head1 SYNOPSIS
+
+B<AVSS09ViperValidator> S<[ B<--help> | B<--man> | B<--version> ]>
+  S<[B<--xmllint> I<location>] [B<--CLEARxsd> I<location>]>
+  S<[B<--gtf>] [B<--frameTol> I<framenbr>]>
+  S<[[B<--write> [I<directory>]]>
+  S<[B<--WriteMemDump> [I<mode>] [B<--skipScoringSequenceMemDump>]]]>
+  S<[B<--ForceFilename> I<filename>]>
+  S<I<viper_source_file.xml>[I<transformations>]>
+  S<[I<viper_source_file.xml>[I<transformations>] [I<...>]>
+  
+=head1 DESCRIPTION
+
+B<AVSS09ViperValidator> performs a syntactic and semantic validation of the ViPER XML file(s) provided on the command line. It can I<validate> reference files (see B<--gtf>) as well as system files. It can also rewrite validated files into another directory using the same filename as the original (see B<--write>), write a I<memory representation> of the ViPER file post validation (to fasten reload time), as well as a I<Scoring Sequence> I<memory representation> to be used for scoring.
+
+=head1 PREREQUISITES
+
+
+B<AVSS09ViperValidator> relies on some external software and files, most of which associated with the B<CLEAR> section of B<F4DE>.
+
+=over
+
+=item B<SOFTWARE> 
+
+I<xmllint> (part of I<libxml2>, see S<http://www.xmlsoft.org/>) is required (at least version 2.6.30) to perform the syntactic validation of the source file.
+If I<xmllint> is not available in your PATH, you can specify its location either on the command line (see B<--xmllint>) or by setting the S<CLEAR_XMLLINT> environment variable.
+
+=item B<FILES>
+
+The syntactic validation requires some XML schema files (see the B<CLEARDTScorer> help section for a list of the required files).
+It is possible to specify their location using the B<--xsdpath> option or the B<CLEAR_XSDPATH> environment variable.
+You should not have to specify their location, if you have performed an install and have set the global environment variables.
+
+=item B<GLOBAL ENVIRONMENT VARIABLES>
+
+B<AVSS09ViperValidator> relies on some internal and external Perl libraries to function.
+
+Simply running the B<AVSS09ViperValidator> script should provide you with the list of missing libraries. 
+The following environment variables should be set in order for Perl to use the B<F4DE> libraries:
+
+=over
+
+=item B<F4DE_BASE>
+
+The main variable once you have installed the software, it should be sufficient to run this program.
+
+=item B<F4DE_PERL_LIB>
+
+Allows you to specify a different directory for the B<F4DE> libraries.
+
+=item B<AVSS09_PERL_LIB>
+
+Allows you to specify a different directory for the B<AVSS09> libraries.
+
+=item B<CLEAR_PERL_LIB>
+
+Allows you to specify a different directory for the B<CLEAR> libraries.
+
+=back
+
+=back
+
+=head1 GENERAL NOTES
+
+B<AVSS09ViperValidator> expects that the files can be validated using 'xmllint' against the B<CLEAR> XSD file(s) (see B<--help> for files list).
+
+B<AVSS09ViperValidator> will use the core validation of the B<CLEAR> code and add some specialized checks associated with the B<AVSS09> evaluation.
+
+=head1 OPTIONS
+
+=over
+
+=item B<--CLEARxsd> I<location>
+
+Specify the default location of the required XSD files.
+Can also be set using the B<CLEAR_XSDPATH> environment variable.
+
+=item B<--ForceFilename> I<file>
+
+Replace the I<sourcefile>'s I<filename> value by I<file>.
+
+=item B<--frameTol> I<framenbr>
+
+The frame tolerance allowed for attributes to be outside of the object framespan
+
+=item B<--gtf>
+
+Specify that the files to validate are Reference files (also known as a Ground Truth Files)
+
+=item B<--help>
+
+Display the usage page for this program. Also display some default values and information.
+
+=item B<--man>
+
+Display this man page.
+
+=item B<--skipScoringSequenceMemDump>
+
+Do not generate the I<Scoring Sequence> memory representation when generating the validated ViPER file memory representation.
+This representation can be used by the scoring program.
+
+=item B<--version>
+
+Display B<AVSS09ViperValidator> version information.
+
+=item B<--write> [I<directory>]
+
+Once validation has been completed for a given file, B<AVSS09ViperValidator> will write a new XML representation of this file to either the standard output (if I<directory> is not set), or will create a file with the same name as the input file in I<directory> (if specified).
+
+=item B<--WriteMemDump> [I<mode>]
+
+Write to disk a memory representation of the validated ViPER File.
+This memory representation file can be used as the input of other AVSS09 tools.
+The mode is the file representation to disk and its values and its default can be obtained using the B<--help> option.
+
+=item B<--xmllint> I<location>
+
+Specify the full path location of the B<xmllint> command line tool if not available in your PATH.
+Can also be set using the B<CLEAR_XMLLINT> environment variable.
+
+=back
+
+=head1 TRANSFORMATIONS
+
+Transformations are an easy way to influence the content of the ViPER file.
+
+Syntax: S<[:B<FSshift>][@B<BBmod>][#B<IDadd>]>
+
+=over
+
+=item B<FSshift>
+
+It is the number of frames to add or substract from every framespan within the specified file.
+
+=item B<BBmod>
+
+It is the I<bounding box> modifications of the form S<I<X>+I<Y>xI<M>>, where I<X> and I<Y> add or substract and I<M> multiply.
+
+=item B<IDadd>
+
+It is the number of ID to add or substract to every PERSON ID seen within the specified file.
+
+=back
+
+=head1 USAGE
+
+=item B<AVSS09ViperValidator --xmllint /local/bin/xmllint --TrecVid08xsd /local/F4DE-CVS/data sys_test1.xml>
+
+Will try to validate the I<system> file I<sys_test1.xml> using the I<xmllint> executable located at I</local/bin/xmllint> and the required XSD files found in the I</local/F4DE/data> directory.
+
+=item B<AVSS09ViperValidator --gtf ref_test1.xml:100@-10+20x0.5#5 ref_test2.xml --frameTol 5 --write /tmp --WriteMemDump text>
+
+Will try to validate the I<reference> files I<ref_test1.xml> and I<ref_test2.xml> using a tolerance of 5 frames for attributes to be ouside of the object framespan.
+For I<ref_test1.xml>, it will also add 100 frames to each framespan found within the file, modify the I<bounding box> by substracting 10 from each X coordinate, adding 20 to each Y, and multiplying each resulting coordinate by 0.5, and add 5 to each object ID seen. 
+It will then write to the I</tmp> directory a XML rewrite of both files, as well as a ViPER file memory represenation (in text format) and a scoring sequence memory representation.
+
+=item B<AVSS09ViperValidator sys_test1.xml sys_test2.xl --ForceFilename ff.xml --write /tmp --WriteMemDump gzip --skipScoringSequenceMemDump>
+
+Will try to validate the I<system> files I<sys_test1.xml> and I<sys_test2.xml>, changing for files the I<sourcefile filename> within to I<ff.xml>.
+Then write into the I</tmp> directory both files and their ViPER file memory represenation (in compressed format, to save some disk space). 
+
+=head1 BUGS
+
+Please send bug reports to <nist_f4de@nist.gov>
+
+=head1 AUTHORS
+
+Martial Michel <martial.michel@nist.gov>
+
+=cut
+
 ########################################
 
 sub set_usage {
@@ -266,27 +455,28 @@ sub set_usage {
   my $tmp=<<EOF
 $versionid
 
-Usage: $0 [--help] [--version] [--gtf] [--xmllint location] [--CLEARxsd location] [--frameTol framenbr] [--write [directory] [--WriteMemDump [mode] [--skipSequenceMemDump]] [--ForceFilename file] viper_source_file.xml[transformations] [viper_source_file.xml[transformations] [...]]
+Usage: $0 [--help | --man | --version] [--xmllint location] [--CLEARxsd location] [--gtf] [--frameTol framenbr] [--write [directory] [--WriteMemDump [mode] [--skipScoringSequenceMemDump]] [--ForceFilename file] viper_source_file.xml[transformations] [viper_source_file.xml[transformations] [...]]
 
 Will perform a semantic validation of the AVSS09 Viper XML file(s) provided.
 
  Where:
+  --help          Print this usage information and exit
+  --man           Print a more detailled manual page and exit (same as running: $mancmd)
+  --version       Print version number and exit
   --gtf           Specify that the file to validate is a Ground Truth File
   --xmllint       Full location of the \'xmllint\' executable (can be set using the $xmllint_env variable)
   --CLEARxsd  Path where the XSD files can be found (can be set using the $xsdpath_env variable)
-  --frameTol      The frame tolerance allowed for attributes to be outside of the object framespan
+  --frameTol      The frame tolerance allowed for attributes to be outside of the object framespan (default: $frameTol)
   --ForceFilename  Specify that all files loaded refers to the same 'sourcefile' file
   --write         Once processed in memory, print a new XML dump of file read (or to the same filename within the command line provided directory if given)
   --WriteMemDump  Write a memory representation of validated ViPER Files that can be used by the Scorer and Merger tools. Two modes possible: $wmd (1st default)
-  --skipSequenceMemDump  Do not perform the sequence MemDump (useful for scoring) 
-  --version       Print version number and exit
-  --help          Print this usage information and exit
+  --skipScoringSequenceMemDump  Do not perform the Scoring Sequence MemDump (which can be used for scoring) 
 
 Transformations syntax: [:FSshift][\@BBmod][#IDadd]
 where: 
 - FSshift is the number of frames to add or substract from every framespan within this file
 - BBmod is the bounding box modifications of the form X+YxM, ie X and Y add or substract and M multiply (example: @-10+20x0.5 will substract 10 from each X coordinate, add 20 to each Y, and multiply each resulting coordinate by 0.5)
-- IDadd is the number of ID to add or substract to every PERSON ID sen within this file
+- IDadd is the number of ID to add or substract to every PERSON ID seen within this file
 
 Note:
 - This prerequisite that the file can be been validated using 'xmllint' against the 'CLEAR.xsd' file
@@ -298,3 +488,5 @@ EOF
 
   return $tmp;
 }
+
+######################################## THE END
