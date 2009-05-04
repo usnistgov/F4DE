45f056f3784793fd33adb7be7ba55006c1e8f548
Martial Michel <martial.michel@nist.gov>
2009-05-04 20:24:36 +0000

    Make use of MMisc

--- CLEAR07/test/CLEARTRViperValidator/CLEARTRViperValidator_tester.pl
+++ CLEAR07/test/CLEARTRViperValidator/CLEARTRViperValidator_tester.pl
@@ -3,9 +3,10 @@
 
 use strict;
 use F4DE_TestCore;
+use MMisc;
 
 my $validator = shift @ARGV;
-error_quit("ERROR: Validator ($validator) empty or not an executable\n")
+MMisc::error_quit("ERROR: Validator ($validator) empty or not an executable\n")
   if (($validator eq "") || (! -f $validator) || (! -x $validator));
 my $mode = shift @ARGV;
 
@@ -16,21 +17,23 @@ my $testr = 0;
 my $tn = "";
 my $td = "";
 
+##
 $tn = "test-1a";
 $td = "BN_TR";
 $testr += &do_simple_test($tn, $td, "(BN TextRec: Reference files)", "*.gtf", "", "-D BN -w -f 15", "res-$tn.txt");
 
+##
 $tn = "test-1b";
 $td = "BN_TR";
 $testr += &do_simple_test($tn, $td, "(BN TextRec: System Submissions)", "", "BBN_VByblos_2006_Test_BNews_TREng_08_01.rdf", "-D BN -w -f 15", "res-$tn.txt");
 
+#####
+
 if ($testr == $totest) {
-  ok_quit("All test ok\n\n");
-} else {
-  error_quit("Not all test ok\n\n");
+  MMisc::ok_quit("All test ok\n\n");
 }
 
-die("You should never see this :)");
+MMisc::error_quit("Not all test ok\n\n");
 
 ##########
 
@@ -47,24 +50,11 @@ sub do_simple_test {
     $command = "$validator $files $params";
   }
   else {
-    error_quit("No input files for validator\n\n");
+    MMisc::error_quit("No input files for validator\n\n");
   }
 
   $totest++;
 
+  $testname =~ s%\-%%;
   return(F4DE_TestCore::run_simpletest($testname, $subtype, $command, $res, $mode));
-}
-
-#####
-
-sub ok_quit {
-  print @_;
-  exit(0);
-}
-
-#####
-
-sub error_quit {
-  print @_;
-  exit(1);
 }
