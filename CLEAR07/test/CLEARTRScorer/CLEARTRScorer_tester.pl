b42281d0c29c4c07087c9a0164335e90515b8093
Martial Michel <martial.michel@nist.gov>
2009-05-04 22:17:12 +0000

    Make use of MMisc

--- CLEAR07/test/CLEARTRScorer/CLEARTRScorer_tester.pl
+++ CLEAR07/test/CLEARTRScorer/CLEARTRScorer_tester.pl
@@ -3,9 +3,10 @@
 
 use strict;
 use F4DE_TestCore;
+use MMisc;
 
 my $scorer = shift @ARGV;
-error_quit("ERROR: Scorer ($scorer) empty or not an executable\n")
+MMisc::error_quit("ERROR: Scorer ($scorer) empty or not an executable\n")
   if (($scorer eq "") || (! -f $scorer) || (! -x $scorer));
 my $mode = shift @ARGV;
 
@@ -16,21 +17,23 @@ my $testr = 0;
 my $tn = "";
 my $td = "";
 
+##
 $tn = "test-1a";
 $td = "BN_TR";
 $testr += &do_simple_test($tn, $td, "(BN TextRec: regular non-binary area thresholding)", "*.gtf", "*.rdf", "-D BN -E Area", "res-$tn.txt");
 
+##
 $tn = "test-1b";
 $td = "BN_TR";
 $testr += &do_simple_test($tn, $td, "(BN TextRec: regular non-binary point thresholding)", "*.gtf", "*.rdf", "-D BN -E Point", "res-$tn.txt");
 
+#####
+
 if ($testr == $totest) {
-  ok_quit("All test ok\n\n");
-} else {
-  error_quit("Not all test ok\n\n");
+  MMisc::ok_quit("All test ok\n\n");
 }
 
-die("You should never see this :)");
+MMisc::error_quit("Not all test ok\n\n");
 
 ##########
 
@@ -42,19 +45,6 @@ sub do_simple_test {
   my $command = "$scorer $fsf -g $frf $params -f 15";
   $totest++;
 
+  $testname =~ s%\-%%;
   return(F4DE_TestCore::run_simpletest($testname, $subtype, $command, $res, $mode));
-}
-
-#####
-
-sub ok_quit {
-  print @_;
-  exit(0);
-}
-
-#####
-
-sub error_quit {
-  print @_;
-  exit(1);
 }
