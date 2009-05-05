a6f53249ec841829489d1dfce907999a6728390c
Martial Michel <martial.michel@nist.gov>
2009-05-05 22:01:05 +0000

    - Use 'F4DE_BASE' as the only env var - extended manual entry

--- AVSS09/tools/AVSS09ViPERValidator/AVSS09ViPERValidator.pl
+++ AVSS09/tools/AVSS09ViPERValidator/AVSS09ViPERValidator.pl
@@ -37,18 +37,14 @@ my $versionid = "AVSS09 ViPER XML Validator Version: $version";
 # Check we have every module (perl wise)
 
 ## First insure that we add the proper values to @INC
-my ($f4b, $f4bv, $avpl, $avplv, $clearpl, $clearplv, $f4depl, $f4deplv);
+my ($f4b, @f4bv);
 BEGIN {
   $f4b = "F4DE_BASE";
-  $f4bv = (defined $ENV{$f4b}) ? $ENV{$f4b} . "/lib": "/lib";
-  $avpl = "AVSS09_PERL_LIB";
-  $avplv = $ENV{$avpl} || "../../lib";
-  $clearpl = "CLEAR_PERL_LIB";
-  $clearplv = $ENV{$clearpl} || "../../../CLEAR07/lib"; # Default is relative to this tool's default path
-  $f4depl = "F4DE_PERL_LIB";
-  $f4deplv = $ENV{$f4depl} || "../../../common/lib";  # Default is relative to this tool's default path
+  push @f4bv, (exists $ENV{$f4b}) 
+    ? ($ENV{$f4b} . "/lib") 
+      : ("../../lib", "../../../CLEAR07/lib", "../../../common/lib");
 }
-use lib ($avplv, $clearplv, $f4deplv, $f4bv);
+use lib (@f4bv);
 
 sub eo2pe {
   my @a = @_;
@@ -60,28 +56,24 @@ sub eo2pe {
 ## Then try to load everything
 my $ekw = "ERROR"; # Error Key Work
 my $have_everything = 1;
-my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable (if you did an install, otherwise your $avpl, $clearpl and $f4depl environment variables).";
+my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable.";
 my $warn_msg = "";
 
-# MMisc (part of this tool)
-unless (eval "use MMisc; 1") {
-  my $pe = &eo2pe($@);
-  &_warn_add("\"AVSS09ViperFile\" is not available in your Perl installation. ", $partofthistool, $pe);
-  $have_everything = 0;
+# Part of this tool
+foreach my $pn ("MMisc", "AVSS09ViperFile") {
+  unless (eval "use $pn; 1") {
+    my $pe = &eo2pe($@);
+    &_warn_add("\"$pn\" is not available in your Perl installation. ", $partofthistool, $pe);
+    $have_everything = 0;
+  }
 }
 
-# AVSS09ViperFile (part of this tool)
-unless (eval "use AVSS09ViperFile; 1") {
-  my $pe = &eo2pe($@);
-  &_warn_add("\"AVSS09ViperFile\" is not available in your Perl installation. ", $partofthistool, $pe);
-  $have_everything = 0;
-}
-
-# Getopt::Long (usualy part of the Perl Core)
-unless (eval "use Getopt::Long; 1") {
-  &_warn_add("\"Getopt::Long\" is not available on your Perl installation. ",
-             "Please see \"http://search.cpan.org/search?mode=module&query=getopt%3A%3Along\" for installation information\n");
-  $have_everything = 0;
+# usualy part of the Perl Core
+foreach my $pn ("Getopt::Long") {
+  unless (eval "use $pn; 1") {
+    &_warn_add("\"$pn\" is not available on your Perl installation. ", "Please look it up on CPAN [http://search.cpan.org/]\n");
+    $have_everything = 0;
+  }
 }
 
 # Something missing ? Abort
@@ -96,8 +88,7 @@ Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
 ########################################
 # Options processing
 
-my $xmllint_env = "CLEAR_XMLLINT";
-my $xsdpath_env = "CLEAR_XSDPATH";
+my $xmllint_env = "F4DE_XMLLINT";
 my $mancmd = "perldoc -F $0";
 my @ok_md = ("gzip", "text"); # Default is gzip / order is important
 my $frameTol = 0;
@@ -106,9 +97,7 @@ my $usage = &set_usage();
 # Default values for variables
 my $isgtf = 0;
 my $xmllint = MMisc::get_env_val($xmllint_env, "");
-my $xsdpath = MMisc::get_env_val($xsdpath_env, "../../../CLEAR07/data");
-$xsdpath = "$f4bv/data" 
-  if (($f4bv ne "/lib") && ($xsdpath eq "../../../CLEAR07/data"));
+my $xsdpath = (exists $ENV{$f4b}) ? ($ENV{$f4b} . "/data") : "../../../CLEAR07/data";
 my $show = 0;
 my $forceFilename = "";
 my $writeback = -1;
@@ -288,10 +277,9 @@ B<AVSS09ViperValidator> S<[ B<--help> | B<--man> | B<--version> ]>
   
 =head1 DESCRIPTION
 
-B<AVSS09ViperValidator> performs a syntactic and semantic validation of the ViPER XML file(s) provided on the command line. It can I<validate> reference files (see B<--gtf>) as well as system files. It can also rewrite validated files into another directory using the same filename as the original (see B<--write>), write a I<memory representation> of the ViPER file post validation (to fasten reload time), as well as a I<Scoring Sequence> I<memory representation> to be used for scoring.
+B<AVSS09ViperValidator> performs a syntactic and semantic validation of the ViPER XML file(s) provided on the command line. It can I<validate> reference files (see B<--gtf>) as well as system files. It can also rewrite validated files into another directory using the same filename as the original (see B<--write>), write a I<memory representation> of the ViPER file post validation (to hasten reload time), as well as a I<Scoring Sequence> I<memory representation> to be used for scoring.
 
 =head1 PREREQUISITES
-
 
 B<AVSS09ViperValidator> relies on some external software and files, most of which associated with the B<CLEAR> section of B<F4DE>.
 
@@ -300,40 +288,17 @@ B<AVSS09ViperValidator> relies on some external software and files, most of whic
 =item B<SOFTWARE> 
 
 I<xmllint> (part of I<libxml2>, see S<http://www.xmlsoft.org/>) is required (at least version 2.6.30) to perform the syntactic validation of the source file.
-If I<xmllint> is not available in your PATH, you can specify its location either on the command line (see B<--xmllint>) or by setting the S<CLEAR_XMLLINT> environment variable.
+If I<xmllint> is not available in your PATH, you can specify its location either on the command line (see B<--xmllint>) or by setting the S<F4DE_XMLLINT> environment variable to the full path location of the I<xmllint> executable.
 
 =item B<FILES>
 
 The syntactic validation requires some XML schema files (see the B<CLEARDTScorer> help section for a list of the required files).
-It is possible to specify their location using the B<--xsdpath> option or the B<CLEAR_XSDPATH> environment variable.
+It is possible to specify their location using the B<--xsdpath> option.
 You should not have to specify their location, if you have performed an install and have set the global environment variables.
 
 =item B<GLOBAL ENVIRONMENT VARIABLES>
 
-B<AVSS09ViperValidator> relies on some internal and external Perl libraries to function.
-
-Simply running the B<AVSS09ViperValidator> script should provide you with the list of missing libraries. 
-The following environment variables should be set in order for Perl to use the B<F4DE> libraries:
-
-=over
-
-=item B<F4DE_BASE>
-
-The main variable once you have installed the software, it should be sufficient to run this program.
-
-=item B<F4DE_PERL_LIB>
-
-Allows you to specify a different directory for the B<F4DE> libraries.
-
-=item B<AVSS09_PERL_LIB>
-
-Allows you to specify a different directory for the B<AVSS09> libraries.
-
-=item B<CLEAR_PERL_LIB>
-
-Allows you to specify a different directory for the B<CLEAR> libraries.
-
-=back
+Once you have installed the software, setting B<F4DE_BASE> to the installation location, and extending your B<PATH> to include B<$F4DE_BASE/bin> should be sufficient for the tools to find their components.
 
 =back
 
@@ -350,7 +315,6 @@ B<AVSS09ViperValidator> will use the core validation of the B<CLEAR> code and ad
 =item B<--CLEARxsd> I<location>
 
 Specify the default location of the required XSD files.
-Can also be set using the B<CLEAR_XSDPATH> environment variable.
 
 =item B<--ForceFilename> I<file>
 
@@ -394,7 +358,7 @@ The mode is the file representation to disk and its values and its default can b
 =item B<--xmllint> I<location>
 
 Specify the full path location of the B<xmllint> command line tool if not available in your PATH.
-Can also be set using the B<CLEAR_XMLLINT> environment variable.
+Can also be set using the B<F4DE_XMLLINT> environment variable.
 
 =back
 
@@ -445,6 +409,12 @@ Please send bug reports to <nist_f4de@nist.gov>
 
 Martial Michel <martial.michel@nist.gov>
 
+=head1 COPYRIGHT 
+
+This software was developed at the National Institute of Standards and Technology by employees of the Federal Government in the course of their official duties.  Pursuant to Title 17 Section 105 of the United States Code this software is not subject to copyright protection within the United States and is in the public domain. It is an experimental system.  NIST assumes no responsibility whatsoever for its use by any party.
+
+THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+
 =cut
 
 ########################################
@@ -465,7 +435,7 @@ Will perform a semantic validation of the AVSS09 Viper XML file(s) provided.
   --version       Print version number and exit
   --gtf           Specify that the file to validate is a Ground Truth File
   --xmllint       Full location of the \'xmllint\' executable (can be set using the $xmllint_env variable)
-  --CLEARxsd  Path where the XSD files can be found (can be set using the $xsdpath_env variable)
+  --CLEARxsd  Path where the XSD files can be found
   --frameTol      The frame tolerance allowed for attributes to be outside of the object framespan (default: $frameTol)
   --ForceFilename  Specify that all files loaded refers to the same 'sourcefile' file
   --write         Once processed in memory, print a new XML dump of file read (or to the same filename within the command line provided directory if given)
