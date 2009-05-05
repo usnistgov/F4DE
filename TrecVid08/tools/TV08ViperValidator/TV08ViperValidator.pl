0d0f757a250d49460f1433f4fd369fae4bc3ad63
Martial Michel <martial.michel@nist.gov>
2009-05-05 22:29:09 +0000

    manual typo fix

--- TrecVid08/tools/TV08ViperValidator/TV08ViperValidator.pl
+++ TrecVid08/tools/TV08ViperValidator/TV08ViperValidator.pl
@@ -692,8 +692,6 @@ Once you have installed the software, setting B<F4DE_BASE> to the installation l
 
 =back
 
-=back
-
 =head1 GENERAL NOTES
 
 B<TV08ViperValidator> expect that the files can be validated using 'xmllint' against the TrecVid08 XSD file(s) (see B<--help> for files list).
