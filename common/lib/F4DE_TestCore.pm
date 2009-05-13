199c976e08a3a1ac2d2b6e88c8e662db7abcca28
Martial Michel <martial.michel@nist.gov>
2009-05-13 22:15:57 +0000

    Added "elapsed" time prints as specified from 'F4DE_TEST_OPTIONS' environment variable, as well as functions to access time values

--- common/lib/F4DE_TestCore.pm
+++ common/lib/F4DE_TestCore.pm
@@ -2,9 +2,11 @@ package F4DE_TestCore;
 
 use strict;
 use MMisc;
+use Time::HiRes;
 
 my $env_mmcadd = "F4DE_TEST_MMC_ADD";
 my $env_testonly = "F4DE_TEST_ONLY";
+my $env_testoptions = "F4DE_TEST_OPTIONS";
 
 my $magicmode = "makecheckfiles"; # And the magic word is ...
 my $magicmode_comp = "makecompcheckfiles";
@@ -15,6 +17,10 @@ my $mmc_add = $ENV{$env_mmcadd} || "-comp";
 my $dev = "F4DE_TestCore default error value";
 my $lts = 10;                   # lines to show
 
+my @testoptions_list = ( "elapsedtime" );
+my $processed_testoptions = 0;
+my $show_elapsed = 0;
+
 ####################
 
 sub get_magicmode_key { return($magicmode); }
@@ -24,6 +30,7 @@ sub get_magicmode_comp_key { return($magicmode_comp); }
 
 sub get_mmcadd_env { return($env_mmcadd); }
 sub get_testonly_env { return($env_testonly); }
+sub get_testoptions_env { return($env_testoptions); }
 
 #####
 
@@ -45,6 +52,55 @@ sub set_testonly_env {
       if (MMisc::is_blank($val));
 
   $env_testonly = $val;
+}
+
+#####
+
+sub set_testoptions_env {
+  my $val = shift @_;
+
+  MMisc::error_quit("Can not set \"$env_testoptions\" to an empty value")
+      if (MMisc::is_blank($val));
+
+  $env_testoptions = $val;
+}
+
+#####
+
+sub show_authorized_test_options { return(join(" ", @testoptions_list)); }
+
+##########
+
+sub is_elapsedtime_on {
+  &check_testoptions_env();
+  return(1) if ($show_elapsed);
+
+  return(0);
+}
+
+#####
+
+sub get_currenttime { return([Time::HiRes::gettimeofday()]); }
+
+#####
+
+sub get_elapsedtime { return(Time::HiRes::tv_interval(shift @_)); }
+
+##########
+
+sub check_testoptions_env {
+  return() if (! exists $ENV{$env_testoptions});
+  return() if ($processed_testoptions);
+
+  my $v = $ENV{$env_testoptions};
+
+  my $t = $testoptions_list[0];
+  $show_elapsed = 1 if ($v =~ s%$t%%i);
+
+  MMisc::error_quit("Unknown options(s)  (" . MMisc::clean_begend_spaces($v) . ") for \"$env_testoptions\", authorized options: " . &show_authorized_test_options())
+      if (! MMisc::is_blank($v));
+
+  $processed_testoptions = 1;
 }
 
 ###############
@@ -100,24 +156,26 @@ sub _run_core {
     $ofile = MMisc::get_tmpfilename();
   }
 
+  my $t0 = &get_currenttime();
   my ($retcode, $run, @toshow) = &make_syscall($ofile, $cmd);
+  my $elapsed = &get_elapsedtime($t0);
 
   my $tst = join("\n  ## ", @toshow);
 
-  return(0, $ofile, $tst) if (($retcode >> 8) != $erv);
+  return(0, $ofile, $tst, $elapsed) if (($retcode >> 8) != $erv);
   # 'perldoc -f system': to get the actual exit value, shift right by eight
 
-  return(1, $ofile, $tst) if (&_is_magic($mode));
+  return(1, $ofile, $tst, $elapsed) if (&_is_magic($mode));
 
   my $runfile = &_get_filec($ofile);
   my $resfile = &_get_filec($cmp2resfile);
   unlink($ofile);
 
-  return(0) if (($runfile eq $dev) || ($resfile eq $dev));
+  return(0, "", "", $elapsed) if (($runfile eq $dev) || ($resfile eq $dev));
 
-  return(1, $ofile, $tst) if ($runfile eq $resfile);
+  return(1, $ofile, $tst, $elapsed) if ($runfile eq $resfile);
 
-  return(0, $ofile, $tst);
+  return(0, $ofile, $tst, $elapsed);
 }
 
 ##########
@@ -181,26 +239,31 @@ sub run_simpletest {
     return(1);
   }
 
-  my ($rv, $ofile, $toshow) = &_run_core($cmd, $cmp2resfile, $mode, $erv);
+  &check_testoptions_env();
+
+  my ($rv, $ofile, $toshow, $elapsed) = &_run_core($cmd, $cmp2resfile, $mode, $erv);
+
+  my $add = "";
+  $add .= " [Elapsed: $elapsed seconds]" if ($show_elapsed);
 
   if (&_is_magic($mode)) {
     print "makecheckfile ... ";
     if ($rv == 0) {
-      print "##### failed ! ##### (see $ofile)\n";
+      print "##### failed ! ##### (see $ofile)$add\n";
       print "## Last $lts lines of stdout run:\n  ## $toshow\n";
       return(0);
     }
-    print "ok [wrote: $ofile]\n";
+    print "ok [wrote: $ofile]$add\n";
     print "## Last $lts lines of stdout run:\n  ## $toshow\n";
     return(1);
   }
 
   if ($rv == 0) {
-    print "failed\n";
+    print "failed$add\n";
     return(0);
   }
 
-  print "OK\n";
+  print "OK$add\n";
   return(1);
 }
 
@@ -289,26 +352,31 @@ sub run_complextest {
     return(1);
   }
 
-  my ($rv, $ofile, $toshow) = &_run_core($cmd, $cmp2resfile, $mode);
+  &check_testoptions_env();
+
+  my ($rv, $ofile, $toshow, $elapsed) = &_run_core($cmd, $cmp2resfile, $mode);
+
+  my $add = "";
+  $add .= " [Elapsed: $elapsed seconds]" if ($show_elapsed);
 
   if (&_is_magic($mode)) {
     print "makecheckfile ... ";
     if (($rv == 0) || (! &_mcfgetf($mode, @sfiles))) {
-      print "##### failed ! ##### (see $ofile)\n";
+      print "##### failed ! ##### (see $ofile)$add\n";
       print "## Last $lts lines of stdout run:\n  ## $toshow\n";
       return(0);
     }
-    print "ok [wrote: $ofile]\n";
+    print "ok [wrote: $ofile]$add\n";
     print "## Last $lts lines of stdout run:\n  ## $toshow\n";
     return(1);
   }
 
   if ( ($rv == 0) || (! &_cmp_sfc_dfc(@sfiles)) ) {
-    print "failed\n";
+    print "failed$add\n";
     return(0);
   }
 
-  print "OK\n";
+  print "OK$add\n";
   return(1);
 }
 
