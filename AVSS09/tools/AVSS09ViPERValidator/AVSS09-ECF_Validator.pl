2c951f9aa21ce60bf94041ab388814e6a81f0102
Martial Michel <martial.michel@nist.gov>
2009-05-22 01:53:38 +0000

    Prototype ECF validation tool

---
+++ AVSS09/tools/AVSS09ViPERValidator/AVSS09-ECF_Validator.pl
@@ -0,0 +1,196 @@
+#!/usr/bin/env perl
+
+# AVSS09 ECF Validator
+#
+# Author:    Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "AVSS09 ECF Validator" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+use strict;
+
+# Note: Designed for UNIX style environments (ie use cygwin under Windows).
+
+##########
+# Version
+
+# $Id$
+my $version     = "0.1b";
+
+if ($version =~ m/b$/) {
+  (my $cvs_version = '$Revision$') =~ s/[^\d\.]//g;
+  $version = "$version (CVS: $cvs_version)";
+}
+
+my $versionid = "AVSS09 ECF Validator Version: $version";
+
+##########
+# Check we have every module (perl wise)
+
+## First insure that we add the proper values to @INC
+my ($f4b, @f4bv);
+BEGIN {
+  $f4b = "F4DE_BASE";
+  push @f4bv, (exists $ENV{$f4b}) 
+    ? ($ENV{$f4b} . "/lib") 
+      : ("../../lib", "../../../common/lib");
+}
+use lib (@f4bv);
+
+sub eo2pe {
+  my @a = @_;
+  my $oe = join(" ", @a);
+  my $pe = ($oe !~ m%^Can\'t\s+locate%) ? "\n----- Original Error:\n $oe\n-----" : "";
+  return($pe);
+}
+
+## Then try to load everything
+my $have_everything = 1;
+my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable.";
+my $warn_msg = "";
+
+# Part of this tool
+foreach my $pn ("MMisc", "AVSS09ECF") {
+  unless (eval "use $pn; 1") {
+    my $pe = &eo2pe($@);
+    &_warn_add("\"$pn\" is not available in your Perl installation. ", $partofthistool, $pe);
+    $have_everything = 0;
+  }
+}
+
+# usualy part of the Perl Core
+foreach my $pn ("Getopt::Long") {
+  unless (eval "use $pn; 1") {
+    &_warn_add("\"$pn\" is not available on your Perl installation. ", "Please look it up on CPAN [http://search.cpan.org/]\n");
+    $have_everything = 0;
+  }
+}
+
+# Something missing ? Abort
+if (! $have_everything) {
+  print "\n$warn_msg\nERROR: Some Perl Modules are missing, aborting\n";
+  exit(1);
+}
+
+# Use the long mode of Getopt
+Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
+
+########################################
+# Options processing
+
+my $xmllint_env = "F4DE_XMLLINT";
+my $usage = &set_usage();
+
+# Default values for variables
+my $xmllint = MMisc::get_env_val($xmllint_env, "");
+my $xsdpath = (exists $ENV{$f4b}) ? ($ENV{$f4b} . "/lib/data") : "../../data";
+
+my $show = 0;
+
+# Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz  #
+# Used: A                                h             v x    #
+
+my %opt;
+GetOptions
+  (
+   \%opt,
+   'help',
+   'version',
+   'xmllint=s'       => \$xmllint,
+   'AVSSxsd=s'       => \$xsdpath,
+   # Hiden Option(s)
+   'X_show_internals'  => \$show,
+  ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
+
+MMisc::ok_quit("\n$usage\n") if ($opt{'help'});
+MMisc::ok_quit("$versionid\n") if ($opt{'version'});
+
+MMisc::error_quit("Not enough arguments\n$usage\n") if (scalar @ARGV == 0);
+
+##############################
+# Main processing
+my $ntodo = scalar @ARGV;
+my $ndone = 0;
+foreach my $tmp (@ARGV) {
+  my ($ok, $object) = &load_file($tmp);
+  next if (! $ok);
+
+  $ndone++;
+}
+
+print("All files processed (Validated: $ndone | Total: $ntodo)\n");
+
+MMisc::error_quit("Not all files processed succesfuly") if ($ndone != $ntodo);
+MMisc::ok_quit("\nDone\n");
+
+########## END
+
+sub valok {
+  my ($fname, $txt) = @_;
+
+  print "$fname: $txt\n";
+}
+
+#####
+
+sub valerr {
+  my ($fname, $txt) = @_;
+  foreach (split(/\n/, $txt)){ 
+    &valok($fname, "[ERROR] $_");
+  }
+}
+
+##########
+
+sub load_file {
+  my ($tmp) = @_;
+
+  my $err = MMisc::check_file_r($tmp);
+  if (! MMisc::is_blank($err)) {
+    &valerr($tmp, "skipping: $err");
+    return(0, ());
+  }
+  
+  # Prepare the object
+  my $object = new AVSS09ECF();
+  MMisc::error_quit("While trying to set \'xmllint\' (" . $object->get_errormsg() . ")")
+    if ( (! MMisc::is_blank($xmllint)) && (! $object->set_xmllint($xmllint)) );
+  MMisc::error_quit("While trying to set \'TrecVid08xsd\' (" . $object->get_errormsg() . ")")
+    if ( (! MMisc::is_blank($xsdpath)) && (! $object->set_xsdpath($xsdpath)) );
+  MMisc::error_quit("While setting \'file\' ($tmp) (" . $object->get_errormsg() . ")")
+    if ( ! $object->set_file($tmp) );
+
+  # Validate
+  if (! $object->validate()) {
+    &valerr($tmp, $object->get_errormsg());
+    return(0, ());
+  }
+
+  &valok($tmp, "validates");
+
+  # This is really if you are a debugger
+  print("** Memory Representation:\n", $object->_display()) if ($show);
+
+  return(1, $object);
+}
+
+########################################
+
+sub _warn_add {
+  $warn_msg .= "[Warning] " . join(" ", @_) . "\n";
+}
+
+#####
+
+sub set_usage {
+  return("");
+}
