ae18523bc5ab1c55fc49d159c1e76fb2e4529830
Martial Michel <martial.michel@nist.gov>
2009-05-26 18:12:03 +0000

    moved load_ViperFile to AVSS09HelperFunctions

--- AVSS09/test/AVSS09ViPERValidator/_special_test1.pl
+++ AVSS09/test/AVSS09ViPERValidator/_special_test1.pl
@@ -6,10 +6,11 @@ use strict;
 
 use MMisc;
 use AVSS09ViperFile;
+use AVSS09HelperFunctions;
 
 my $xsdpath = "../../../CLEAR07/data";
 my ($retstatus, $object, $msg) = 
-    AVSS09ViperFile::load_ViperFile(1, "../common/test_file3.clear.xml", 0, "", $xsdpath);
+    AVSS09HelperFunctions::load_ViperFile(1, "../common/test_file3.clear.xml", 0, "", $xsdpath);
 
 MMisc::error_quit("ERROR: $msg")
   if (! $retstatus);
@@ -148,7 +149,7 @@ sub obj_print {
       if (! $object->write_XML($fname, 1, ""));
 
   my ($retstatus, $obj, $msg) = 
-    AVSS09ViperFile::load_ViperFile(1, $fname, 0, "", $xsdpath);
+    AVSS09HelperFunctions::load_ViperFile(1, $fname, 0, "", $xsdpath);
 
   MMisc::error_quit("ERROR reloading rewritten file: $msg")
       if (! $retstatus);
