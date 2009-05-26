4b33c46c394004882c2f79010ad06d56b44de85e
Martial Michel <martial.michel@nist.gov>
2009-05-26 22:09:21 +0000

    new prototypes (and 'new' function)

--- common/lib/CSVHelper.pm
+++ common/lib/CSVHelper.pm
@@ -22,6 +22,8 @@ package CSVHelper;
 
 use strict;
 
+use MErrorH;
+use MMisc;
 use Text::CSV;
 
 my $version     = "0.1b";
@@ -33,9 +35,11 @@ if ($version =~ m/b$/) {
 
 my $versionid = "CSVHelper.pm Version: $version";
 
-########## No 'new' ... only functions to be useful
+########################################
 
-sub get_csv_handler {
+## Constructor
+sub new {
+  my ($class) = shift @_;
   my $qc = shift @_;
   my $sc = shift @_;
 
@@ -43,13 +47,7 @@ sub get_csv_handler {
   $options{always_quote} = 1;
   $options{binary} = 1;
 
-  my $csv = undef;
-
-  return($csv)
-    if ((! MMisc::is_blank($qc)) && (length($qc) > 1));
-
-  return($csv)
-    if ((! MMisc::is_blank($sc)) && (length($sc) > 1));
+  my $csvh = undef;
 
   if (! MMisc::is_blank($qc)) {
     $options{quote_char}  = $qc;
@@ -59,49 +57,120 @@ sub get_csv_handler {
   $options{sep_char} = $sc
     if (! MMisc::is_blank($sc));
 
-  $csv = Text::CSV->new(\%options);
+  $csvh = Text::CSV->new(\%options);
 
-  return($csv);
+  my $errortxt = "";
+  if (! defined $csvh) {
+    $errortxt = "Problem creating CSV handler: " . Text::CSV->error_diag();
+  }
+
+  my $errormsg = new MErrorH("CSVHelper");
+  $errormsg->set_errormsg($errortxt);
+
+  my $self =
+    {
+     csvh            => $csvh,
+     col_nbr         => undef,
+     errormsg        => $errormsg,
+    };
+
+  bless $self;
+  return($self);
 }
 
 #####
 
-sub error { return(! MMisc::is_blank(&get_errormsg())); }
+sub set_number_of_columns {
+  my ($self, $n) = @_;
+
+  return(0) if ($self->error());
+  
+  return($self->_set_error_and_return("\'column number\' can not be less than 1", 0))
+    if ($n < 1);
+
+  return(1);
+}
 
 #####
 
-sub get_errormsg { return(Text::CSV->error_diag()); }
+sub get_number_of_columns {
+  my ($self) = @_;
+  return($self->{col_nbr}); 
+}
 
 #####
 
-sub array2csvtxt {
-  my ($ch, @array) = @_;
+sub array2csvline {
+  my ($self, @array) = @_;
 
-  return(undef)
-    if (! defined $ch);
+  my $cn  = $self->{col_nbr};
+  return($self->_set_error_and_return("Given number of elements in array (" . scalar @array . ") is different from expected number ($cn)", undef))
+    if ((defined $cn) && ($cn != scalar @array));
 
-  return(undef)
+  my $ch = $self->{csvh};
+  return($self->_set_error_and_return
+         ("Problem adding elements to CSV: " 
+          . $ch->error_diag() . " (" . $ch->error_input() . ")"
+          , undef))
     if (! $ch->combine(@array));
 
   my $txt = $ch->string();
+
   return($txt);
 }
 
 #####
 
-sub csvtxt2array {
-  my ($ch, $value) = @_;
+sub csvline2array {
+  my ($self, $value) = @_;
 
-  return(undef)
-    if (! defined $ch);
-
-  return(undef)
+  my $ch = $self->{csvh};
+  return($self->_set_error_and_return
+         ("Problem obtaining array from CSV line: " 
+          . $ch->error_diag() . " (" . $ch->error_input() . ")"
+          , undef))
     if (! $ch->parse($value));
 
   my @columns = $ch->fields();
+  my $cn  = $self->{col_nbr};
+  return($self->_set_error_and_return("Number of elements in extracted array (" . scalar @columns . ") is different from expected number ($cn)", undef))
+    if ((defined $cn) && ($cn != scalar @columns));
+
   return(@columns);
 }
 
-########################################
+############################################################
+
+sub _set_errormsg {
+  my ($self, $txt) = @_;
+  $self->{errormsg}->set_errormsg($txt);
+}
+
+#####
+
+sub get_errormsg {
+  my ($self) = @_;
+  return($self->{errormsg}->errormsg());
+}
+
+#####
+
+sub error {
+  my ($self) = @_;
+  return($self->{errormsg}->error());
+}
+
+#####
+
+sub _set_error_and_return {
+  my $self = shift @_;
+  my $errormsg = shift @_;
+
+  $self->_set_errormsg($errormsg);
+
+  return(@_);
+}
+
+############################################################
 
 1;
