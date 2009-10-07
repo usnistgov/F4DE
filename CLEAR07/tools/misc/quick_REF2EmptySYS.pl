909b77376667d6bd1f1f0ca3cff512673bafc804
Martial Michel <martial.michel@nist.gov>
2009-10-07 19:09:23 +0000

    Initial commit

---
+++ CLEAR07/tools/misc/quick_REF2EmptySYS.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/env perl
+
+# Quick transformation from REF to Empty SYS
+#
+# Author:    Martial Michel
+#
+# This software was developed at the National Institute of Standards and Technology by
+# employees and/or contractors of the Federal Government in the course of their official duties.
+# Pursuant to Title 17 Section 105 of the United States Code this software is not subject to 
+# copyright protection within the United States and is in the public domain.
+#
+# "Quick transformation from REF to Empty SYS" is an experimental system.
+# NIST assumes no responsibility whatsoever for its use by any party.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST MAKES NO EXPRESS
+# OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER, INCLUDING MERCHANTABILITY,
+# OR FITNESS FOR A PARTICULAR PURPOSE.
+
+use strict;
+
+# Note: Designed for UNIX style environments (ie use cygwin under Windows).
+
+##########
+# Version
+
+# $Id$
+my $version     = "0.1b";
+
+if ($version =~ m/b$/) {
+  (my $cvs_version = '$Revision$') =~ s/[^\d\.]//g;
+  $version = "$version (CVS: $cvs_version)";
+}
+
+my $versionid = "Quick transformation from REF to Empty SYS Version: $version";
+
+##########
+# Check we have every module (perl wise)
+
+## First insure that we add the proper values to @INC
+my ($f4b, @f4bv);
+BEGIN {
+  $f4b = "F4DE_BASE";
+  push @f4bv, (exists $ENV{$f4b}) 
+    ? ($ENV{$f4b} . "/lib") 
+      : ("../../lib", "../../../common/lib");
+}
+use lib (@f4bv);
+
+sub eo2pe {
+  my @a = @_;
+  my $oe = join(" ", @a);
+  my $pe = ($oe !~ m%^Can\'t\s+locate%) ? "\n----- Original Error:\n $oe\n-----" : "";
+  return($pe);
+}
+
+## Then try to load everything
+my $have_everything = 1;
+my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable.";
+my $warn_msg = "";
+
+# Part of this tool
+foreach my $pn ("MMisc", "MtXML") {
+  unless (eval "use $pn; 1") {
+    my $pe = &eo2pe($@);
+    &_warn_add("\"$pn\" is not available in your Perl installation. ", $partofthistool, $pe);
+    $have_everything = 0;
+  }
+}
+
+# usualy part of the Perl Core
+foreach my $pn ("Getopt::Long") {
+  unless (eval "use $pn; 1") {
+    &_warn_add("\"$pn\" is not available on your Perl installation. ", "Please look it up on CPAN [http://search.cpan.org/]\n");
+    $have_everything = 0;
+  }
+}
+
+# Something missing ? Abort
+if (! $have_everything) {
+  print "\n$warn_msg\nERROR: Some Perl Modules are missing, aborting\n";
+  exit(1);
+}
+
+# Use the long mode of Getopt
+Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
+
+########################################
+# Options processing
+
+my $usage = "$0 infile outfile";
+MMisc::ok_quit("\n$usage\n") if (scalar @ARGV == 0);
+
+# Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz #
+# Used:                                                      #
+
+my %opt = ();
+GetOptions
+  (
+   \%opt,
+   'help',
+   'version',
+  ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
+MMisc::ok_quit("\n$usage\n") if ($opt{'help'});
+MMisc::ok_quit("$versionid\n") if ($opt{'version'});
+
+MMisc::ok_quit("\n$usage\n") if (scalar @ARGV != 2);
+
+##########
+
+my $etxt= "ERROR_____TEXT";
+
+my $if = shift @ARGV;
+my $of = shift @ARGV;
+
+my $err = MMisc::check_file_r($if);
+MMisc::error_quit("Problem witn input file [$if]: $err") 
+  if (! MMisc::is_blank($err));
+
+my $slurp = MMisc::slurp_file($if);
+MMisc::error_quit("Problem slurping file [$if]") if (! defined $slurp);
+
+$slurp =~ s%^(.+</file>)%%s;
+my $header = $1;
+MMisc::error_quit("Could not extract header") if (MMisc::is_blank($header));
+
+$slurp =~ s%(</sourcefile>.+)$%%s;
+my $trailer = $1;
+MMisc::error_quit("Could not extract trailer") if (MMisc::is_blank($trailer));
+ 
+my $iframes = MtXML::get_named_xml_section_with_inline_content
+  ("object", "name=\"I-FRAMES\"", \$slurp, $etxt);
+MMisc::error_quit("Could not extract Iframes") if ($iframes eq $etxt);
+
+my $frames = $iframes;
+$frames =~ s%I-FRAMES%FRAME%;
+
+MMisc::error_quit("Problem writing output file [$of]")
+  if (! MMisc::writeTo($of, "", 1, 0, "$header\n$iframes\n$frames\n$trailer\n"));
+
+MMisc::ok_exit("Done");
