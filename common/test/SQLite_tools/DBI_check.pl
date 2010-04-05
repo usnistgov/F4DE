e4dcd713a18e3bd53b7e128de0f3ad6b46da3060
Martial Michel <martial.michel@nist.gov>
2010-04-05 21:52:12 +0000

    Added pre tests + DBI/SQLite checks

---
+++ common/test/SQLite_tools/DBI_check.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use strict;
+use MMisc;
+
+use DBI;
+use MtSQLite;
+
+my $err = 0;
+
+#####
+print "\n** SQLite command line location: ";
+
+my ($err, $sqlite, $sv) = MtSQLite::get_sqlitecmd();
+
+if (MMisc::is_blank($sqlite)) {
+  print "** could not find \'sqlite3\' in your PATH **\n";
+  $err++;
+} else {
+  print "$sqlite";
+  if (! MMisc::is_blank($err)) {
+    print " ** Problem with version: $err **";
+    $err++;
+  }
+  print " [$sv]" if (! MMisc::is_blank($sv));
+  print "\n";
+}
+
+#####
+print "\n** DBI's SQLite driver: ";
+
+my @driver_names = DBI->available_drivers();
+
+if (grep(m%^SQLite$%, @driver_names)) {
+  print "OK\n";
+} else {
+  print "** SQLite not available on your DBI installation**\n";
+  $err++;
+}
+
+##########
+MMisc::error_quit("Not all tests ok")
+  if ($err);
+
+MMisc::ok_quit();
---
+++ common/test/SQLite_tools/pre_tests.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+# -*- mode: Perl; tab-width: 2; indent-tabs-mode: nil -*- # For Emacs
+
+use strict;
+use MMisc;
+
+my $err = 0;
+
+##########
+print "** Checking for Perl Required Packages:\n";
+my $ms = 1;
+
+$ms = &_chkpkg("DBI", "SQL::Abstract");
+if ($ms > 0) {
+  print "  ** ERROR: Not all packages found, you will not be able to run the program (and some F4DE package will most likely fail this step), install the missing packages and re-run the checks\n\n";
+  $err++;
+} else {
+  print "  Found all packages\n\n";
+}
+
+##########
+print "** Checking for F4DE Perl Required Packages:\n";
+my $ms = 1;
+
+$ms = &_chkpkg("MtSQLite", "DETCurveGnuplotRenderer");
+if ($ms > 0) {
+  print "  ** ERROR: Not all packages found, you will not be able to run the program\n\n";
+  $err++;
+} else {
+  print "  Found all packages\n\n";
+}
+
+##########
+print "** Checking for gnuplot : ";
+
+my ($err, $gnuplot, $gv) = DETCurveGnuplotRenderer::get_gnuplotcmd();
+if (MMisc::is_blank($err)) {
+  print "$gnuplot [$gv]\n\n";
+} else {
+  print "  ** $err **\n\n";
+  $err++;
+}
+
+####################
+MMisc::error_quit("\nSome issues, fix before attempting to run make check again\n") if ($err);
+
+MMisc::ok_quit("\n** Pre-requisite testing done\n\n");
+
+####################
+
+sub _chkpkg {
+  my @tocheck = @_;
+
+  my $ms = scalar @tocheck;
+  foreach my $i (@tocheck) {
+    print "- $i : ";
+    my $v = MMisc::check_package($i);
+    my $t = $v ? "ok" : "**missing**";
+    print "$t\n";
+    $ms -= $v;
+  }
+
+  return($ms);
+}
