b59dd6b96038a9f26379220d4b33a418ed3531ee
Martial Michel <martial.michel@nist.gov>
2010-05-04 19:50:51 +0000

    Checking for all 'common' packages

--- common/test/pre_tests.pl
+++ common/test/pre_tests.pl
@@ -20,6 +20,42 @@ if ($ms > 0) {
   print "  Found all packages\n\n";
 }
 
+print "[F4DE Common Packages]\n";
+$ms = &_chkpkg
+  (
+   # common/lib
+   "AutoTable",
+   "BarPlot",
+   "BipartiteMatch",
+   "CSVHelper",
+   "DETCurve",
+   "DETCurveGnuplotRenderer",
+   "DETCurveSet",
+   "F4DE_TestCore",
+   "Levenshtein",
+   "MErrorH",
+   "MMisc",
+   "MetricFuncs",
+   "MetricNormLinearCostFunct",
+   "MetricTestStub",
+   "MtSQLite",
+   "MtXML",
+   "PropList",
+   "SimpleAutoTable",
+   "TextTools",
+   "TrialSummaryTable",
+   "TrialsFuncs",
+   "TrialsTestStub",
+   "ViperFramespan",
+   "xmllintHelper"
+  );
+if ($ms > 0) {
+  print "  ** ERROR: Not all packages found, you will not be able to run the programs, please install the missing ones\n\n";
+  $err++;
+} else {
+  print "  Found all packages\n\n";
+}
+
 ##########
 print "** Recommended Perl Packages:\n";
 $ms = &_chkpkg("Text::CSV_XS");
