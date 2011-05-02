7248d2bfc8691df0c257eaa8f58034710b74b1ab
Martial Michel <martial.michel@nist.gov>
2011-05-02 22:19:33 +0000

    Updated to match changes to 'DEVA_filter'

--- DEVA/test/common/filter1.sql
+++ DEVA/test/common/filter1.sql
@@ -1,2 +1,2 @@
-SELECT system.TrialID FROM system INNER JOIN md WHERE system.TrialID=md.TrialID AND Decision>"1.5" AND color="red";
+INSERT OR ABORT INTO ResultsTable ( TrialID ) SELECT system.TrialID FROM system INNER JOIN md WHERE system.TrialID=md.TrialID AND Decision>"1.5" AND color="red";
 
