817acf842b4ac40c4353edaa19e71611b209b204
Martial Michel <martial.michel@nist.gov>
2011-05-25 22:33:24 +0000

    Added 'Duplicates', 'warnColumns', 'otherColumnName', 'extraReplace' command line options

--- common/tools/CSVUtil/injector.pl
+++ common/tools/CSVUtil/injector.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 use strict;
+#use Data::Dumper;
 
 ##########
 # Check we have every module (perl wise)
@@ -62,16 +63,24 @@ MMisc::error_quit($usage) if (scalar @ARGV == 0);
 
 # Default values for variables
 my $addCol = 0;
+my $dupl = 0;
+my $warnok = 0;
+my %ocm = ();
+my @er = ();
 
 # Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz  #
-# Used:                           a      h                    #
+# Used:    D                      a   e  h              w     #
 
 my %opt = ();
 GetOptions
   (
    \%opt,
    'help',
-   'addColumns' => \$addCol,
+   'addColumns'  => \$addCol,
+   'Duplicates'  => \$dupl,
+   'warnColumns' => \$warnok,
+   'otherColumnName=s' => \%ocm,
+   'extraReplace=s' => \@er,
   ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
 MMisc::ok_quit("\n$usage\n") if (($opt{'help'}) || (scalar @ARGV == 0));
 
@@ -80,9 +89,11 @@ MMisc::error_quit($usage)
 
 my ($in, $rin, $out) = @ARGV;
 
-my ($replace_mk, $racn, %replace) = &load_replace($rin);
+my $replace_mk = "";
 my @repl_colname = ();
-foreach my $c (@$racn) { push @repl_colname, $c; }
+my %replace = ();
+&load_replace($rin, \$replace_mk, \@repl_colname, \%replace);
+foreach my $if (@er) { &load_replace($if, \$replace_mk, \@repl_colname, \%replace); }
 
 open IFILE, "<$in"
   or MMisc::error_quit("Problem opening input file ($in) : $!");
@@ -105,6 +116,7 @@ while (my $line = <IFILE>) {
   MMisc::error_quit("Problem with input CSV : " . $icsvh->get_errormsg() . "\n[Line $sh : $line]")
       if ($icsvh->error());
 
+  my @out = ();
   if ($sh == 1) { # Header
     &get_colm(@inh);
     $inlc = scalar @inh;
@@ -118,19 +130,25 @@ while (my $line = <IFILE>) {
         push @inh, $k;
         next;
       }
-      MMisc::error_quit("Required key ($k) not found");
+      MMisc::error_quit("Required key ($k) not found") if (! $warnok);
+      MMisc::warn_print("Expected column key ($k) not found");
     }
     $inlc = scalar @inh;
     $ocsvh->set_number_of_columns($inlc);
+    @out = [@inh];
   } else {
-    @inh = &fixline($replace_mk, $inlc, @inh);
+    @out = &fixline($replace_mk, $inlc, @inh);
   }
 
-  my $otxt = $ocsvh->array2csvline(@inh);
-  MMisc::error_quit("Problem with output CSV : " . $ocsvh->get_errormsg() . "\n[Line $sh : $line]")
-      if ($ocsvh->error());
-
-  print OFILE "$otxt\n";
+  my $otxt = "";
+  for (my $i = 0; $i < scalar @out; $i++) {
+    my @tmp = @{$out[$i]};
+    $otxt .= $ocsvh->array2csvline(@tmp);
+    $otxt .= "\n";
+    MMisc::error_quit("Problem with output CSV : " . $ocsvh->get_errormsg() . "\n[Line $sh : $line]")
+        if ($ocsvh->error());
+  }
+  print OFILE "$otxt";
 
   $sh++;
 }
@@ -141,11 +159,17 @@ $sh--; # Remove orig
 print "\n** " . scalar(keys %seen_mk) . " Master Keys Found\n";
 if (scalar(keys %seen_mk) > 0) {
   foreach my $col (@repl_colname) {
-    print "  - \'$col\' replaced " . $repl_count{$col} . " times\n";
+    my $k = (! exists $repl_count{$col}) ? 0 : $repl_count{$col};
+    print "  - \'$col\' replaced $k times\n";
   }
 }
 
 print "\n** " . scalar(keys %unseen_mk) . " Master Keys NOT Found\n";
+
+foreach my $mk (keys %seen_mk) {
+  delete $replace{$mk};
+}
+print "\n** " . scalar(keys %replace) . " Master Keys from REPLACE not seen:" . join(" ", sort __num keys(%replace)) . "\n";
 
 MMisc::ok_quit("\nDone (processed $sh lines)\n");
 
@@ -170,27 +194,38 @@ sub fixline {
 
   if (! exists $replace{$mk}) {
     $unseen_mk{$mk}++;
-    return(@in);
+    return([@in]);
   }
 
-  foreach my $col (@repl_colname) {
-    my $cv = $in[$colm{$col}];
-    my $rv = $replace{$mk}{$col};
-    next if ($cv eq $rv); # same values
-    $in[$colm{$col}] = $replace{$mk}{$col};
-    $repl_count{$col}++;
+  my @out = ();
+  my $rc = 0;
+  for (my $i = 0; $i < scalar @{$replace{$mk}}; $i++) {
+    my %tmp_replace = %{${$replace{$mk}}[$i]};
+    my @tmp = @in;
+    foreach my $col (@repl_colname) {
+      next if (! exists $colm{$col});
+      my $cv = $tmp[$colm{$col}];
+      my $rv = $tmp_replace{$col};
+      next if ($cv eq $rv); # same values
+      $tmp[$colm{$col}] = $tmp_replace{$col};
+      $repl_count{$col}++;
+    }
+    push @out, [@tmp];
+    $rc++;
   }
- 
+#  print("[***] " . Dumper(\@out) . "\n") if ($rc > 1);
+
   $seen_mk{$mk}++;
   delete $unseen_mk{$mk};
 
-  return(@in);
+  return(@out);
 }
 
 ##########
 
 sub load_replace {
-  my ($if) = @_;
+  my ($if, $rmk, $rra, $rrh) = @_;
+  my %seen = ();
 
   open RIFILE, "<$if"
     or MMisc::error_quit("Problem opening Replacement input file ($if) : $!");
@@ -199,57 +234,92 @@ sub load_replace {
 
   my $sh = 1;
   my $rmc = "";
-  my %repl = ();
   my @col_name = ();
   while (my $line = <RIFILE>) {
     my @inh = $ricsvh->csvline2array($line);
     MMisc::error_quit("Problem with input CSV : " . $ricsvh->get_errormsg() . "\n[Line $sh : $line]")
         if ($ricsvh->error());
-
+    
     if ($sh == 1) { # Header
       MMisc::error_quit("Need at least two columns in Replacement CSV file to work, found ". scalar @inh) if (scalar @inh < 2);
-
+      
       $ricsvh->set_number_of_columns(scalar @inh);
       
-      $rmc = $inh[0];
-      @col_name = @inh[1..$#inh];
       my %tmp = ();
-      foreach my $c (@col_name) {
+      for (my $i = 0; $i < scalar @inh; $i++) {
+        my $c = $inh[$i];
+#        print "[$c] ";
         MMisc::error_name("Column name ($c) has to be unique and non empty")
             if ((MMisc::is_blank($c)) || (++$tmp{$c} > 1));
+        if (exists $ocm{$c}) {
+          print "  (seen $c column, renaming as " . $ocm{$c} . ")\n";
+          $inh[$i] = $ocm{$c};
+        }
+      }
+#      die();
+      $rmc = $inh[0];
+      MMisc::error_quit("Master key differs [$rmc] vs [$$rmk]")
+          if ((! MMisc::is_blank($$rmk)) && ($rmc ne $$rmk));
+      $$rmk = $rmc;
+
+      @col_name = @inh[1..$#inh];
+      foreach my $c (@col_name) {
+        push(@{$rra}, $c) if (! grep(m%^$c$%, @{$rra}));
       }
       $sh++;
       next;
     }
 
     my $mk = shift @inh;
-    MMisc::error_quit("Master Key uniqueness in doubt ($mk)")
-        if (exists $repl{$mk});
-    for (my $i = 0; $i < scalar @inh; $i++) {
-      $repl{$mk}{$col_name[$i]} = $inh[$i];
+    if (exists $$rrh{$mk}) {
+      MMisc::error_quit("Master Key is not unique ($mk)")
+          if (! $dupl);
+#      print("  -- FYI: Master Key not unique: $mk\n");
+      $seen{$mk}++;
     }
+    my %tmp = ();
+    for (my $i = 0; $i < scalar @inh; $i++) {
+      $tmp{$col_name[$i]} = $inh[$i];
+    }
+    push @{$$rrh{$mk}}, \%tmp;
 
     $sh++;
   }
   close RIFILE;
 
-  return($rmc, \@col_name, %repl);
+  my @keys = keys %seen;
+  if (scalar @keys > 0) {
+    print " -- FYI: The following Master Keys are not unique: ";
+    foreach my $key (sort __num @keys) {
+      print "$key (x" . (1 + $seen{$key}) . ") ";
+    }
+    print "\n";
+  }
+
+  return(1);;
 }
+
+## 
+
+sub __num { $a <=> $b; }
 
 ####################
 
 sub set_usage{
   my $tmp=<<EOF
 
-$0 [--help] [--addColumns] infile.csv replacefile.csv outfile.csv
+$0 [--help] [--addColumns] [--Duplicates] [--warnColumns] [--otherColumnName infilename=outfilename] [--extraReplace replacefile.csv] infile.csv replacefile.csv outfile.csv
 
 Replace columns from input file by columns from replacefile.
-Match is done on the first column in replacefile (conisdered a primary key), and replaced column names have to match.
+Match is done on the first column in replacefile considered the 'master key' (must be a primary key unless \'--Duplicates\' is used), and replaced column names have to match.
 
 Where:
   --help        This help message
   --addColumns  If a column name in 'replacefile.csv' does not exist in 'infile.csv', it will be added to 'outfile.csv'
-
+  --Duplicates  Allow duplicate master keys and will replace all instances found with all candidates
+  --warnColumns If a column is in infile but not in outfile, warn instead of exiting with error
+  --otherColumnName  Allow renaming of infile column's name to its corresponding value in outfile
+  --extraReplace   Add extra replacement entries
 EOF
 ;
 
