52742b106e0def06cbf7f08c54e69ab26bef172c
Martial Michel <martial.michel@nist.gov>
2011-06-09 22:57:48 +0000

    Merge from Jon's "F4DE-2dot2-BRANCH-20110527" branch: Changed the name of the DETCurve to make it backward compatable

--- TrecVid08/tools/TV08Scorer/TV08Scorer.pl
+++ TrecVid08/tools/TV08Scorer/TV08Scorer.pl
@@ -523,7 +523,7 @@ foreach my $event (@all_events) {
   my $trials = $all_trials{$event};
   my $metric = $all_metric{$event};
 #  print "** Computing DET Curves for event: $event\n";
-  my $det = new DETCurve($trials, $metric, "Event $event: ". $sysTitle, [()], $gzipPROG);
+  my $det = new DETCurve($trials, $metric, "$event Event". $sysTitle, [()], $gzipPROG);
   print $det->getMessages()
     if (! MMisc::is_blank($det->getMessages()));
   my $rtn = $detSet->addDET($event." Event", $det);
