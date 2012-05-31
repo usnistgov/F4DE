567f2244b016cae08b5dfb63c2e9289d1ef69e22
Martial Michel <martial.michel@nist.gov>
2012-05-31 17:23:09 +0000

    Disclaimer update

--- common/tools/DETUtil/DETUtil.pl
+++ common/tools/DETUtil/DETUtil.pl
@@ -10,9 +10,15 @@
 # Technology by employees of the Federal Government in the course of
 # their official duties.  Pursuant to Title 17 Section 105 of the United
 # States Code this software is not subject to copyright protection within
-# the United States and is in the public domain. It is an experimental
-# system.  NIST assumes no responsibility whatsoever for its use by any
-# party.
+# the United States and is in the public domain. 
+# It is an experimental system.  
+# NIST assumes no responsibility whatsoever for its use by any party, and makes no guarantees,
+# expressed or implied, about its quality, reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.  This software can be
+# redistributed and/or modified freely provided that any derivative works bear some notice
+# that they are derived from it, and any modified versions bear some notice that they
+# have been modified.
 # 
 # THIS SOFTWARE IS PROVIDED "AS IS."  With regard to this software, NIST
 # MAKES NO EXPRESS OR IMPLIED WARRANTY AS TO ANY MATTER WHATSOEVER,
