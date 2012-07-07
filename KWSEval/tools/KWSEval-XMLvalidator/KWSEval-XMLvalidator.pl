279fc615f6ebf9f3e17eea6722ff3aa923f844fe
Martial Michel <martial.michel@nist.gov>
2012-07-07 02:42:03 +0000

    Use actual "object" load function to validate file

--- KWSEval/tools/KWSEval-XMLvalidator/KWSEval-XMLvalidator.pl
+++ KWSEval/tools/KWSEval-XMLvalidator/KWSEval-XMLvalidator.pl
@@ -67,7 +67,7 @@ my $partofthistool = "It should have been part of this tools' files. Please chec
 my $warn_msg = "";
 
 # Part of this tool
-foreach my $pn ("MMisc", "xmllintHelper") {
+foreach my $pn ("MMisc", "KWSecf", "TermList", "KWSList") {
   unless (eval "use $pn; 1") {
     my $pe = &eo2pe($@);
     &_warn_add("\"$pn\" is not available in your Perl installation. ", $partofthistool, $pe);
@@ -95,19 +95,14 @@ Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
 ########################################
 # Options processing
 
-my $xmllint_env = "F4DE_XMLLINT";
-my $toolb = "xsdxmllint";
-my @xsdfilesl = ('KWSEval-ecf.xsd', 'KWSEval-kwslist.xsd', 'KWSEval-termlist.xsd'); # order is important
 my $usage = &set_usage();
 MMisc::ok_quit("\n$usage\n") if (scalar @ARGV == 0);
 
 # Default values for variables
 
 # Av  : ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz #
-# Used:                    T   X      e  h  k        t vwx   #
+# Used:                               e  h  k        t vwx   #
 
-my $xmllint = "";
-my $xsdpath = "";
 my $issome = -1;
 my $writeback = -1;
 my $tool = "";
@@ -118,60 +113,96 @@ GetOptions
    \%opt,
    'help',
    'version',
-   'Tool=s'    => \$tool,
-   'xmllint=s' => \$xmllint,
-   'Xsdpath=s' => \$xsdpath,
-   'ecf'       => sub {$issome = 0;},
-   'kwslist'   => sub {$issome = 1;},
-   'termlist'  => sub {$issome = 2;},
+   'ecf'       => sub {MMisc::error_quit("Can not specify more than one mode") if ($issome != -1); $issome = 0;},
+   'kwslist'   => sub {MMisc::error_quit("Can not specify more than one mode") if ($issome != -1); $issome = 1;},
+   'termlist'  => sub {MMisc::error_quit("Can not specify more than one mode") if ($issome != -1); $issome = 2;},
    'write:s'   => \$writeback,
   ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n\n$usage\n");
 
 MMisc::ok_quit("\n$usage\n") if ($opt{'help'});
 MMisc::ok_quit("$versionid\n") if ($opt{'version'});
 
+MMisc::warn_print("\'write\' is not currently implemented")
+  if ($writeback != -1);
+
 MMisc::error_quit("Did not specify validation type, must be either \'--ecf\', \'--kwslist\' or \'--termlist\'")
   if ($issome == -1);
 
-$tool = (MMisc::is_blank($tool)) ? 
-  ((exists $ENV{$f4b}) ? $ENV{$f4b} . "/bin/$toolb" : dirname(abs_path($0)) . "/../../../common/tools/xmllintTools/$toolb.pl")
-  : $tool;
-my $err = MMisc::check_file_x($tool);
-MMisc::error_quit("Problem with tool ($tool): $err")
-  if (! MMisc::is_blank($err));
+my $ndone = 0;
+my $ntodo = 0;
+while (my $tmp = shift @ARGV) {
+  $ntodo++;
 
-$xmllint = MMisc::get_env_val($xmllint_env, "")
-  if (MMisc::is_blank($xmllint));
+  my ($ok, $object) = &load_file($tmp);
+  next if (! $ok);
 
-$xsdpath = (MMisc::is_blank($xsdpath)) ? ((exists $ENV{$f4b}) ? ($ENV{$f4b} . "/lib/data") : (dirname(abs_path($0)) . "/../../data")) : $xsdpath;
+  $ndone++;
+}
+print "All files processed (Validated: $ndone | Total: $ntodo)\n\n";
+MMisc::error_exit()
+  if ($ndone != $ntodo);
 
-print "** Performing initial check\n";
-my ($rc, $so, $se) = &run_tool();
-MMisc::error_quit("Problem with core tool ($tool):\n[stdout]$so\n[stderr]$se\n")
-  if ($rc != 0);
-
-print "** Running all checks (will print result after tool is completed)\n";
-($rc, $so, $se) = &run_tool(@ARGV);
-
-print $so;
-print "\n[stderr]\n$se\n" if (! MMisc::is_blank($se));
-
-MMisc::ok_exit() if ($rc == 0);
-
-MMisc::error_exit();
+MMisc::ok_exit();
 
 ########## END
 
-sub run_tool {
-  my @cmd = ($tool);
-  push @cmd, ('-x', $xmllint) if (! MMisc::is_blank($xmllint));
-  push @cmd, ('-p', $xsdpath);
-  push @cmd, ('-f', $xsdfilesl[$issome]);
-  push @cmd, ('-w') if ($writeback != -1);
-  push @cmd, ($writeback) if (($writeback != -1) && ($writeback ne ""));
-  push @cmd, (@_) if (scalar @_ > 0);
+sub valok { print $_[0] . ": " . $_[1] . "\n"; }
 
-  return(MMisc::do_system_call(@cmd));
+#####
+
+sub valerr {
+  my ($fname, $txt) = @_;
+  foreach (split(/\n/, $txt)) { 
+    &valok($fname, "[ERROR] $_");
+  }
+}
+
+
+sub load_file {
+  my ($err, $object);
+ 
+  if ($issome == 0) {
+    ($err, $object) = &load_ECF($_[0]);
+  } elsif ($issome == 1) {
+    ($err, $object) = &load_KWSList($_[0]);
+  } elsif ($issome == 2) {
+    ($err, $object) = &load_TermList($_[0]);
+  } else {
+    MMisc::error_quit("Unknown Mode selected (we should not be here), aborting");
+  }
+
+  my $ok = MMisc::is_blank($err);
+  if ($ok) {
+    &valok($_[0], "validates");
+  } else {
+    &valerr($_[0], $err);
+  }
+
+  return($ok, $object);
+}
+
+##
+
+sub load_ECF {
+  my $object = new KWSecf();
+  my $err = $object->loadFile($_[0]);
+  return($err, $object);
+}
+
+## 
+
+sub load_KWSList {
+  my $object = new KWSList();
+  my $err = $object->loadFile($_[0]);
+  return($err, $object);
+}
+
+## 
+
+sub load_TermList {
+  my $object = new TermList();
+  my $err = $object->loadFile($_[0]);
+  return($err, $object);
 }
 
 ########################################
@@ -182,27 +213,20 @@ sub _warn_add {
 
 ############################################################ Manual
 sub set_usage {
-  my $xsdfiles = join(" ", @xsdfilesl);
   my $tmp=<<EOF
 $versionid
 
 Usage:
-$0 [--help] [--version] [--Tool location] [--xmllint location] [--Xsdpath dirlocation] [--write [directory]] --ecf ecf_file.xml [ecf_file.xml [...]]
-$0 [--help] [--version] [--Tool location] [--xmllint location] [--Xsdpath dirlocation] [--write [directory]] --kwslist kwslist_file.xml [kwslist_file.xml [...]]
-$0 [--help] [--version] [--Tool location] [--xmllint location] [--Xsdpath dirlocation] [--write [directory]] --termlist termlist_file.xml [termlist_file.xml [...]]
+$0 [--help] [--version] [--write [directory]] --ecf ecf_file.xml [ecf_file.xml [...]]
+$0 [--help] [--version][--write [directory]] --kwslist kwslist_file.xml [kwslist_file.xml [...]]
+$0 [--help] [--version][--write [directory]] --termlist termlist_file.xml [termlist_file.xml [...]]
 
-Will validate KWS Eval's ECF, TermList or KWSlist files using Tool ($toolb)
+Will validate KWS Eval's ECF, TermList or KWSlist files
 
 Where:
   --help          Print this usage information and exit
   --version       Print version number and exit
-  --Tool          Location of global XML validator ($toolb)
-  --xmllint       Full location of the \'xmllint\' executable (can be set using the $xmllint_env variable)
-  --Xsdpath       Path where the XSD files can be found
   --write         Once processed in memory, print a new XML dump of file read (or to the same filename within the command line provided directory if given)
-
-Note:
-- 'Xsdpath' files are: $xsdfiles
 
 EOF
     ;
