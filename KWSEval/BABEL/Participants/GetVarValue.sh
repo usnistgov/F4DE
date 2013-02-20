9d5aac90933401284353f1fa1b410c2226addbad
Martial Michel <martial.michel@nist.gov>
2013-02-20 22:16:08 +0000

    Extract a variable's value set from a shell config file

---
+++ KWSEval/BABEL/Participants/GetVarValue.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## Exit with error status
+# call: error_quit errormessage
+error_quit () {
+    echo "ERROR: $1"
+    exit 1
+}
+
+## Check that a file exists, is a file and is readable
+# call: check_file filename
+check_file () {
+  if [ ! -e "$1" ]; then error_quit "File ($1) does not exist"; fi
+  if [ ! -f "$1" ]; then error_quit "File ($1) is not a file"; fi
+  if [ ! -r "$1" ]; then error_quit "File ($1) is not readable"; fi
+}
+
+get_basedir () {
+    wbd=`perl -e 'use Cwd "abs_path"; use File::Basename "dirname"; print dirname(abs_path($ARGV[0]));' $1`
+}
+
+##########
+
+
+usage()
+{
+cat <<EOF
+Usage: $0 ConfigFile Variable
+prints the Variable values extracted from ConfigFile
+EOF
+}
+
+if  [ $# -lt 2 ]; then usage; error_quit "Not enough arguments on the command line, quitting" ; fi
+
+##########
+
+
+conf="$1"
+check_file "$conf"
+source "$conf"
+
+var="$2"
+eval echo $`echo $var`
+
+exit 0
+
+
+
