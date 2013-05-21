fb3e8e108a7804ce019a9b371299910bf92c481a
indus <>
2013-05-21 13:46:19 +0000

    Modified the --Info opton to parse the DAF types and report values

--- common/tools/CSVUtil/CSVUtil.pl
+++ common/tools/CSVUtil/CSVUtil.pl
@@ -128,10 +128,41 @@ if (! $at->loadCSV($in, undef, undef, undef, $quoteChar, $sepChar)) {
 }
   
 if (defined($printInfo)){
+  use Statistics::Descriptive;
   print "Information about $in\n";
   print scalar($at->getRowIDs("AsAdded"))." Rows:\n";
   print scalar($at->getColIDs("AsAdded"))." Columns: \n";
-  print "   ".join(",\n   ", $at->getColIDs("AsAdded") )."\n";
+  foreach my $col($at->getColIDs("AsAdded")){
+      my $type = $col; 
+      $type =~ s/^(.*)(_d|_c|_t|_b|ID)$/$2/;
+      $type = "text" if ($type eq $col);
+      print "  $type $col -> ";
+      if ($type =~ /^(_b|_d)$/){
+          my %ht = ();
+          foreach my $row($at->getRowIDs("AsAdded")){ 
+              my $val = $at->getData($col, $row);
+              $ht{$val}++;
+          }
+          foreach my $val(sort keys %ht){ print " $val->".$ht{$val}}
+      } elsif ($type =~ /^(_c)$/){
+          my $stat = Statistics::Descriptive::Full->new();
+          my $unk = 0;
+          foreach my $row($at->getRowIDs("AsAdded")){ 
+              my $val = $at->getData($col, $row);
+              if (! defined($val)){
+                  $unk++;
+              } else {
+                  $stat->add_data($val);
+              }
+          }
+          print "#unk=$unk, #val=".$stat->count().", min=".$stat->min().", mean=".$stat->mean().", median=".$stat->median().", max=".$stat->max();
+      } else {
+          ;
+      }
+      print "\n";
+  }
+  
+  
 }
 
 if (defined($outCSV)){
