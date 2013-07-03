2ab3e2d0c1bf5a085a1ca5666488b268108b534c
Martial Michel <martial.michel@nist.gov>
2013-07-03 21:17:49 +0000

    Fix usage + skip TrialID header line

--- DEVA/tools/MED-SubmissionChecker/MER-SubmissionChecker.sh
+++ DEVA/tools/MED-SubmissionChecker/MER-SubmissionChecker.sh
@@ -3,7 +3,8 @@ completeness_file=$2
 xsd_name="MER13_output_v1.xsd"
 
 if [ -z $submission_dir ]; then
-    echo "Missing first argument, should be submission directory!"
+    echo "$0 MER_submission_dir [completeness_file]"
+#    echo "Missing first argument, should be submission directory!"
     exit 1
 fi
 
@@ -20,7 +21,7 @@ if [ $completeness_file -a -f $completeness_file ]; then
     echo "*Checking submission for completeness*"
     complete_list=`mktemp -t mer_check`
     echo $submission_dir > $complete_list
-    cat $completeness_file | perl -ne 's/["\n]//g;($c, $e) = split(/\./); print "'$submission_dir'/$e\n'$submission_dir'/$e/$_.mer.xml\n"' | sort -u >> $complete_list
+    cat $completeness_file | perl -ne 's/["\n]//g; next if ($_ eq "TrialID");($c, $e) = split(/\./); print "'$submission_dir'/$e\n'$submission_dir'/$e/$_.mer.xml\n"' | sort -u >> $complete_list
 
     diff_results=`mktemp -t mer_diff`
     find $submission_dir | diff - $complete_list > $diff_results
