9a56600d01b978132e13e4599246b0bff5bbc254
Martial Michel <martial.michel@nist.gov>
2013-07-05 22:32:21 +0000

    Added capability to make symbolic links

--- installer.pl
+++ installer.pl
@@ -4,78 +4,128 @@ use strict;
 use Getopt::Long;
 Getopt::Long::Configure(qw(auto_abbrev no_ignore_case));
 
+## First insure that we add the proper values to @INC
+my (@f4bv, $f4d);
+BEGIN {
+  use Cwd 'abs_path';
+  use File::Basename 'dirname';
+  $f4d = dirname(abs_path($0));
+
+  push @f4bv, ("$f4d/common/lib");
+}
+use lib (@f4bv);
+
+use MMisc;
+
+my $usage = "$0 [-removeext] [-xexec] [-link] basedestdir adddestdir files\n\n"
+  . "Will install all \'files\' in \'basedestdir/addestdir\'\n\n"
+  . "Where:\n"
+  . "  -removeext   removes the file extension\n"
+  . "  -xexec       makes the file executable\n"
+  . "  -link        makes symbolic links instead of using rsync\n";
+
 my $remext = 0;
 my $isexec = 0;
+my $makelink = 0;
 GetOptions
   (
    'removeext'  => \$remext,
    'xecec'      => \$isexec,
-  ) or error_quit("Wrong option(s) on the command line, aborting\n");
+   'link'       => \$makelink,
+  ) or MMisc::error_quit("Wrong option(s) on the command line, aborting\n$usage");
 
-error_quit("Usage: $0 [options] basedestdir adddestdir files\n")
+MMisc::error_quit("Not enough arguments\n\n$usage")
   if (scalar @ARGV < 3);
 
-my ($bdd, $destdir, @srcfiles) = @ARGV;
-error_quit("ERROR: Destination dir can not be \'\/\', sorry")
+my ($tbdd, $destdir, @srcfiles) = @ARGV;
+my $bdd = MMisc::get_dir_actual_dir($tbdd);
+MMisc::error_quit("\'basedestdir\' dir can not be \'\/\', sorry")
   if ($bdd eq "/");
-
 $bdd =~ s%/$%%;
 
 # Checks
 my $etxt = "";
-$etxt .= "Directory \'$bdd\' is not a directory" if (! -d $bdd);
-$etxt .= "Directory \'$bdd\' not writable" if (! -w $bdd);
+my $err = MMisc::check_dir_w($bdd);
+$etxt .= "Directory \'$bdd\': $err. "
+  if (! MMisc::is_blank($err));
 foreach my $f (@srcfiles) {
-  $etxt .= "File \'$f\' missing\n" if (! -e $f);
-  $etxt .= "File \'$f\' not a file\n" if (! -f $f);
-  $etxt .= "File \'$f\' readable\n" if (! -r $f);
+  $err = MMisc::check_file_r($f);
+  $etxt .= "File \'$f\': $err. " 
+    if (! MMisc::is_blank($err));
 }
-error_quit($etxt) if ($etxt ne "");
+MMisc::error_quit($etxt) 
+  if (! MMisc::is_blank($etxt));
 
 # Make the destination directory
 my $fdd = "$bdd/$destdir";
-&_mkdir($fdd);
+MMisc::error_quit("Problem creating writable directory ($fdd)")
+  if (! MMisc::make_wdir($fdd));
 foreach my $f (@srcfiles) {
-  &_cp($f, $fdd);
+  &doit($f, $fdd);
 }
 
-ok_quit();
+MMisc::ok_exit();
 
 ##########
 
-sub _mkdir {
-  my $dir = shift @_;
-
-  error_quit("ERROR: Not a directory ($dir)\n") if ((-e $dir) && (! -d $dir));
-  return() if (-e $dir);
-
-  `mkdir -p $dir`;
-
-  error_quit("ERROR: Could not create requested directory ($dir)")
-    if ((! -e $dir) || (! -d $dir));
-}
-
-#####
-
 sub _chmodx {
-  my $dfile = shift @_;
-
+  my $dfile = MMisc::get_file_actual_file(shift @_);
+  
   if ($isexec) {
     return if (-x $dfile);
     `chmod a+x $dfile`;
-    error_quit("ERROR: could not make file ($dfile) executable")
-      if (! -x $dfile);
+    MMisc::error_quit("Problem running \'chmod\' command on $dfile")
+	if ($? != 0);
+    MMisc::error_quit("Could not make file ($dfile) executable")
+	if (! -x $dfile);
   } else {
     return if (! -x $dfile);
     `chmod a-x $dfile`;
-    error_quit("ERROR: could not make file ($dfile) not executable")
-      if (-x $dfile);
+    MMisc::error_quit("Problem running \'chmod\' command on $dfile")
+	if ($? != 0);
+    MMisc::error_quit("Could not make file ($dfile) not executable")
+	if (-x $dfile);
   }
 }
 
 #####
 
-sub _cp {
+sub linkit {
+  my $file = shift @_;
+  my $to  = shift @_;
+
+  my $sfile = $file;
+  $sfile =~ s%^.+/([^/]+)$%$1%;
+
+  my $dfile = $sfile;
+  $dfile =~ s%\.[^\.]+$%% if ($remext);
+
+  my $from = $file;
+  $from =~ s%^(.+)/[^/]+$%$1%;
+
+  # of note: for 'cd' the from/to change, therefore:
+  my $relpath = MMisc::compute_actual_dir_relative_path($to, $from);
+  MMisc::error_quit("Could not compute relative path from ($to) to ($from)")
+      if (! defined $relpath);
+
+  my $here = MMisc::get_pwd();
+  MMisc::error_quit("Could not chdir to destination directory ($to)")
+      if (! chdir($to));
+  
+  if (! -l $dfile) { # nothing present, let us try to create a symlink
+    `ln -s $relpath/$sfile $dfile`;
+    MMisc::error_quit("Problem making a symbolic link from [$file] into [$to]")
+	if ($? != 0);
+  }
+  &_chmodx($dfile);
+
+  MMisc::error_quit("Could not chdir to source directory ($here)")
+      if (! chdir($here));
+}
+
+#####
+
+sub rsyncit {
   my $file = shift @_;
   my $fdd  = shift @_;
 
@@ -84,21 +134,19 @@ sub _cp {
   my $dfile = "$fdd/$fe";
   $dfile =~ s%\.[^\.]+$%% if ($remext);
   `rsync -a $file $dfile`;
-  error_quit("ERROR: copying file ($file -> $dfile)\n")
-    if ((! -e $dfile) && (! -f $dfile));
+  MMisc::error_quit("Problem rsync-ing file [$file] as [$dfile]")
+      if ($? != 0);
+  MMisc::error_quit("ERROR: copying file ($file -> $dfile)\n")
+      if ((! -e $dfile) && (! -f $dfile));
   &_chmodx($dfile);
-}
-
-##########
-
-sub ok_quit {
-  print @_;
-  exit(0);
 }
 
 #####
 
-sub error_quit {
-  print @_;
-  exit(1);
+sub doit {
+  if ($makelink) {
+    &linkit(@_);
+  } else {
+    &cpit(@_);
+  }
 }
