44de3f7c1b013f9eb82d831cd26619c69a2bdeb8
Martial Michel <martial.michel@nist.gov>
2013-07-06 01:06:26 +0000

    - removed F4DE_BASE dependency + removed actual run command line from output

--- CLEAR07/tools/CLEARTRScorer/CLEARTRScorer.pl
+++ CLEAR07/tools/CLEARTRScorer/CLEARTRScorer.pl
@@ -44,16 +44,13 @@ my $versionid = "CLEAR Text Recognition Scorer Version: $version";
 # Check we have every module (perl wise)
 
 ## First insure that we add the proper values to @INC
-my ($f4b, @f4bv, $f4d);
+my (@f4bv, $f4d);
 BEGIN {
   use Cwd 'abs_path';
   use File::Basename 'dirname';
   $f4d = dirname(abs_path($0));
 
-  $f4b = "F4DE_BASE";
-  push @f4bv, (exists $ENV{$f4b}) 
-    ? ($ENV{$f4b} . "/lib") 
-      : ("$f4d/../../lib", "$f4d/../../../common/lib");
+  push @f4bv, ("$f4d/../../lib", "$f4d/../../../common/lib");
 }
 use lib (@f4bv);
 
@@ -66,7 +63,7 @@ sub eo2pe {
 
 ## Then try to load everything
 my $have_everything = 1;
-my $partofthistool = "It should have been part of this tools' files. Please check your $f4b environment variable.";
+my $partofthistool = "It should have been part of this tools' files.";
 my $warn_msg = "";
 
 # Part of this tool
@@ -116,7 +113,7 @@ MMisc::ok_quit("\n$usage\n") if (scalar @ARGV == 0);
 # Default values for variables
 my $gtfs = 0;
 my $xmllint = MMisc::get_env_val($xmllint_env, "");
-my $xsdpath = (exists $ENV{$f4b}) ? ($ENV{$f4b} . "/lib/data") : (dirname(abs_path($0)) . "/../../data");
+my $xsdpath = "$f4d/../../data";
 my $evaldomain      = undef;
 my $eval_type       = undef;
 my $bin             = 0;
@@ -284,7 +281,8 @@ MMisc::error_quit("ERROR: Generating Final Report (". $results->get_errormsg() .
 
 my $param_setting = &get_param_settings();
 
-my $output = $commandline . $param_setting . $tbl;
+#my $output = $commandline . $param_setting . $tbl;
+my $output = $param_setting . $tbl;
 MMisc::error_quit("Problem while trying to \'write\'")
   if (! MMisc::writeTo($writeres, "", 1, 0, $output, "", "** Text Recognition Results:\n"));
 
