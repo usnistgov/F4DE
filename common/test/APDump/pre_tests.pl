20297ec61cace16c121bf5b0d224aac9f3e8b99d
Martial Michel <martial.michel@nist.gov>
2015-04-22 13:37:17 +0000

    Added sqlite3 check

---
+++ common/test/APDump/pre_tests.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+use strict;
+use MMisc;
+
+use MtSQLite;
+
+my $err = 0;
+
+#####
+print "\n** SQLite command line location: ";
+
+my ($err, $sqlite, $sv) = MtSQLite::get_sqlitecmd();
+
+if (MMisc::is_blank($sqlite)) {
+  print "** could not find \'sqlite3\' in your PATH **\n";
+  $err++;
+} else {
+  print "$sqlite";
+  if (! MMisc::is_blank($err)) {
+    print " ** Problem with version: $err **";
+    $err++;
+  }
+  print " [$sv]" if (! MMisc::is_blank($sv));
+  print "\n";
+}
+
+##########
+MMisc::error_quit("Not all tests ok")
+  if ($err);
+
+MMisc::ok_quit();
