98010c727d53c753478ea1bd13d97874f718d4b7
Martial Michel <martial.michel@nist.gov>
2016-02-01 13:58:48 -0500

    Added headers to BABEL components for Perl 5.18

--- KWSEval/BABEL/Participants/InternalSHA_to_RemoteSHA.pl
+++ KWSEval/BABEL/Participants/InternalSHA_to_RemoteSHA.pl
@@ -1,4 +1,8 @@
-#!/usr/bin/env perl
+#!/bin/sh
+#! -*-perl-*-
+eval 'exec env PERL_PERTURB_KEYS=0 PERL_HASH_SEED=0 perl -x -S $0 ${1+"$@"}'
+  if 0;
+
 #
 # $Id$
 #
@@ -7,6 +11,16 @@ use strict;
 
 my (@f4bv, $f4d);
 BEGIN {
+  if ( ($^V ge 5.18.0)
+       && ( (! exists $ENV{PERL_HASH_SEED})
+	    || ($ENV{PERL_HASH_SEED} != 0)
+	    || (! exists $ENV{PERL_PERTURB_KEYS} )
+	    || ($ENV{PERL_PERTURB_KEYS} != 0) )
+     ) {
+    print "You are using a version of perl above 5.16 ($^V); you need to run perl as:\nPERL_PERTURB_KEYS=0 PERL_HASH_SEED=0 perl\n";
+    exit 1;
+  }
+
   use Cwd 'abs_path';
   use File::Basename 'dirname';
   $f4d = dirname(abs_path($0));
